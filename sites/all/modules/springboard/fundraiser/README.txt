8d3184b2c4bc7c1b57bef7a9af6530eaeac4ff55
pcave <pcave@d6f0bc24-8257-48cf-a643-f3625d64e4ef>
2010-02-04 21:47:43 +0000

    Initial checkin of essential files
    git-svn-id: http://jacksonriver.svn.beanstalkapp.com/springboard/trunk@2 d6f0bc24-8257-48cf-a643-f3625d64e4ef

---
+++ sites/all/modules/springboard/fundraiser/README.txt
@@ -0,0 +1,4 @@
+1. Need to patch Ubercart uc_cart.module
+	-fundraiser/uc_cart.module.patch needs to be applied to uc_cart.module
+	
+2. Fundraiser reporting utilizes MySQL views. So the database user will need 'CREATE VIEW' priviledges
---
+++ sites/all/modules/springboard/fundraiser/fundraiser.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+	// make sure cc number and cvv are numeric
+	$('#edit-submitted-credit-card-information-card-number').numeric();
+	$('#edit-submitted-credit-card-information-card-cvv').numeric();
+	$('#edit-submitted-credit-card-information-card-expiration-date-day').hide();
+
+	$('#edit-submitted-donation-quantity').change(function() {
+		$('#total').empty();
+		var total = this.value * $('#webform-component-amount input:radio:checked').val();
+		$('#edit-submitted-donation-quantity').after('<p id="total">Total: $' + total + '</p>');
+	});
+	
+	$('#edit-submitted-donation-other-amount').change(function() {
+		if ($(this).val() != '') {
+			$('#edit-submitted-donation-amount-other').attr('checked', true);
+		}
+	});
+	
+	$('.node .webform-client-form #edit-submit').click(function() {
+	  $(this).hide();
+	  $('.fundraiser_submit_message').hide();
+    var path = Drupal.settings.basePath + 'sites/all/modules/fundraiser/';
+    $(this).after('<div style="clear: right; float: right; padding-right: 167px; height: 16px; width: 16px; background: url(' + path + 'loader.gif) no-repeat left top;"></style>').after('<p style="clear: right; float: right; margin: 0; padding: 0 75px 0.5em 0; text-align: center; width: 200px;">Thank you.</p><p style="clear: right; float: right; margin: 0; padding: 0 75px 1em 0; text-align: center; width: 200px;">Your donation is being processed.</p>');
+	});
+});
