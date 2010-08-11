273924a7bfcee390c3a3e6d43cbd291bdee02eea
jacksonriverTJ <jacksonriverTJ@d6f0bc24-8257-48cf-a643-f3625d64e4ef>
2010-08-11 15:41:30 +0000

    update default ubercart email template to remove order and store language
    git-svn-id: http://jacksonriver.svn.beanstalkapp.com/springboard/trunk@214 d6f0bc24-8257-48cf-a643-f3625d64e4ef

--- sites/all/modules/ubercart/uc_order/templates/customer.itpl.php
+++ sites/all/modules/ubercart/uc_order/templates/customer.itpl.php
@@ -38,7 +38,7 @@
           <td>
 
             <?php if ($thank_you_message) { ?>
-            <p><b><?php echo t('Thanks for your order, [order-first-name]!'); ?></b></p>
+            <p><b><?php echo t('Thank you for your gift, [order-first-name]!'); ?></b></p>
 
             <?php if (isset($_SESSION['new_user'])) { ?>
             <p><b><?php echo t('An account has been created for you with the following details:'); ?></b></p>
@@ -54,7 +54,7 @@
             <table cellpadding="4" cellspacing="0" border="0" width="100%" style="font-family: verdana, arial, helvetica; font-size: small;">
               <tr>
                 <td colspan="2" bgcolor="#006699" style="color: white;">
-                  <b><?php echo t('Purchasing Information:'); ?></b>
+                  <b><?php echo t('Transaction Information:'); ?></b>
                 </td>
               </tr>
               <tr>
@@ -79,10 +79,10 @@
                       </td>
                       <?php if (uc_order_is_shippable($order)) { ?>
                       <td valign="top" width="50%">
-                        <b><?php echo t('Shipping Address:'); ?></b><br />
+                        <b><?php echo t('Mailing Address:'); ?></b><br />
                         [order-shipping-address]<br />
                         <br />
-                        <b><?php echo t('Shipping Phone:'); ?></b><br />
+                        <b><?php echo t('Mailing Phone:'); ?></b><br />
                         [order-shipping-phone]<br />
                       </td>
                       <?php } ?>
@@ -93,7 +93,7 @@
               </tr>
               <tr>
                 <td nowrap="nowrap">
-                  <b><?php echo t('Order Grand Total:'); ?></b>
+                  <b><?php echo t('Transaction Total:'); ?></b>
                 </td>
                 <td width="98%">
                   <b>[order-total]</b>
@@ -108,167 +108,16 @@
                 </td>
               </tr>
 
-              <tr>
-                <td colspan="2" bgcolor="#006699" style="color: white;">
-                  <b><?php echo t('Order Summary:'); ?></b>
-                </td>
-              </tr>
-
-              <?php if (uc_order_is_shippable($order)) { ?>
-              <tr>
-                <td colspan="2" bgcolor="#EEEEEE">
-                  <font color="#CC6600"><b><?php echo t('Shipping Details:'); ?></b></font>
-                </td>
-              </tr>
-              <?php } ?>
-
-              <tr>
-                <td colspan="2">
-
-                  <table border="0" cellpadding="1" cellspacing="0" width="100%" style="font-family: verdana, arial, helvetica; font-size: small;">
-                    <tr>
-                      <td nowrap="nowrap">
-                        <b><?php echo t('Order #:'); ?></b>
-                      </td>
-                      <td width="98%">
-                        [order-link]
-                      </td>
-                    </tr>
-
-                    <?php if ($shipping_method && uc_order_is_shippable($order)) { ?>
-                    <tr>
-                      <td nowrap="nowrap">
-                        <b><?php echo t('Shipping Method:'); ?></b>
-                      </td>
-                      <td width="98%">
-                        [order-shipping-method]
-                      </td>
-                    </tr>
-                    <?php } ?>
-
-                    <tr>
-                      <td nowrap="nowrap">
-                        <?php echo t('Products Subtotal:'); ?>&nbsp;
-                      </td>
-                      <td width="98%">
-                        [order-subtotal]
-                      </td>
-                    </tr>
-
-                    <?php
-                    $context = array(
-                      'revision' => 'themed',
-                      'type' => 'line_item',
-                      'subject' => array(
-                        'order' => $order,
-                      ),
-                    );
-                    foreach ($line_items as $item) {
-                    if ($item['line_item_id'] == 'subtotal' || $item['line_item_id'] == 'total') {
-                      continue;
-                    }?>
-
-                    <tr>
-                      <td nowrap="nowrap">
-                        <?php echo $item['title']; ?>:
-                      </td>
-                      <td>
-                        <?php
-                          $context['subject']['line_item'] = $item;
-                          echo uc_price($item['amount'], $context);
-                        ?>
-                      </td>
-                    </tr>
-
-                    <?php } ?>
-
-                    <tr>
-                      <td>&nbsp;</td>
-                      <td>------</td>
-                    </tr>
-
-                    <tr>
-                      <td nowrap="nowrap">
-                        <b><?php echo t('Total for this Order:'); ?>&nbsp;</b>
-                      </td>
-                      <td>
-                        <b>[order-total]</b>
-                      </td>
-                    </tr>
-
-                    <tr>
-                      <td colspan="2">
-                        <br /><br /><b><?php echo t('Products on order:'); ?>&nbsp;</b>
-
-                        <table width="100%" style="font-family: verdana, arial, helvetica; font-size: small;">
-
-                          <?php if (is_array($order->products)) {
-                            $context = array(
-                              'revision' => 'formatted',
-                              'type' => 'order_product',
-                              'subject' => array(
-                                'order' => $order,
-                              ),
-                            );
-                            foreach ($order->products as $product) {
-                              $price_info = array(
-                                'price' => $product->price,
-                                'qty' => $product->qty,
-                              );
-                              $context['subject']['order_product'] = $product;
-                              $context['subject']['node'] = node_load($product->nid);
-                              ?>
-                          <tr>
-                            <td valign="top" nowrap="nowrap">
-                              <b><?php echo $product->qty; ?> x </b>
-                            </td>
-                            <td width="98%">
-                              <b><?php echo $product->title .' - '. uc_price($price_info, $context); ?></b>
-                              <?php if ($product->qty > 1) {
-                                $price_info['qty'] = 1;
-                                echo t('(!price each)', array('!price' => uc_price($price_info, $context)));
-                              } ?>
-                              <br />
-                              <?php echo t('SKU: ') . $product->model; ?><br />
-                              <?php if (is_array($product->data['attributes']) && count($product->data['attributes']) > 0) {?>
-                              <?php foreach ($product->data['attributes'] as $attribute => $option) {
-                                echo '<li>'. t('@attribute: @options', array('@attribute' => $attribute, '@options' => implode(', ', (array)$option))) .'</li>';
-                              } ?>
-                              <?php } ?>
-                              <br />
-                            </td>
-                          </tr>
-                          <?php }
-                              }?>
-                        </table>
-
-                      </td>
-                    </tr>
-                  </table>
-
-                </td>
-              </tr>
-
               <?php if ($help_text || $email_text || $store_footer) { ?>
               <tr>
                 <td colspan="2">
                   <hr noshade="noshade" size="1" /><br />
 
-                  <?php if ($help_text) { ?>
-                  <p><b><?php echo t('Where can I get help with reviewing my order?'); ?></b><br />
-                  <?php echo t('To learn more about managing your orders on [store-link], please visit our <a href="[store-help-url]">help page</a>.'); ?>
-                  <br /></p>
-                  <?php } ?>
-
                   <?php if ($email_text) { ?>
                   <p><?php echo t('Please note: This e-mail message is an automated notification. Please do not reply to this message.'); ?></p>
 
-                  <p><?php echo t('Thanks again for shopping with us.'); ?></p>
                   <?php } ?>
 
-                  <?php if ($store_footer) { ?>
-                  <p><b>[store-link]</b><br /><b>[site-slogan]</b></p>
-                  <?php } ?>
                 </td>
               </tr>
               <?php } ?>
