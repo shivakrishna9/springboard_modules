cf8242a8a9b6d9484689931222f22038c32a0b25
Phillip Cave <phillip.cave@jacksonriver.com>
2010-11-05 16:45:50 -0400

    [#19] Validate user input [#17] Update list of cc exp years on sustainer form [#21] Clear cancellation reason when someone clicks in the box [#18] Min donation amount on sustainer page

---
+++ fundraiser/sustainer.js
@@ -0,0 +1,21 @@
+/* javascript functions for sustainer management page */
+$(document).ready(function() {
+  // clear out the cancellation reason when it receives the focus for the first time
+  $('#edit-reason').one('focus', function() {
+    $(this).val('');
+  });
+  
+  /* Disable submit buttons on the 3 main forms when submitting */
+  $('#fundraiser-donation-amount-form').submit(function() {
+    $('input[type=submit]', this).attr('disabled', 'disabled');
+  });
+  
+  $('#fundraiser-billing-update-form').submit(function() {
+    $('input[type=submit]', this).attr('disabled', 'disabled');
+  });
+  
+  $('#fundraiser-cancel-form').submit(function() {
+    $('input[type=submit]', this).attr('disabled', 'disabled');
+  });
+  
+});
