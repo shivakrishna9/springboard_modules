129dbe8afa6bd02da39766cebe66daa2539e80da
Phillip Cave <phillip.cave@jacksonriver.com>
2011-02-21 13:45:06 -0500

    Add tokenized confirmation page

---
+++ fundraiser/templates/fundraiser-confirmation.tpl.php
@@ -0,0 +1,30 @@
+<?php
+// $Id: fundraiser-confirmation.tpl.php,v 1.0 2011/02/17 22:05:27 pcave Exp $
+
+/**
+ * @file
+ * Customize confirmation screen after successful donation.
+ *
+ * This file may be renamed "fundraiser-confirmation-[nid].tpl.php" to target a
+ * specific donation form on your site. Or you can leave it
+ * "fundraiser-confirmation.tpl.php" to affect all donation confirmations on your
+ * site.
+ *
+ * Available variables:
+ * - $node: The node object for this donation form.
+ * - $confirmation_message: The confirmation message input by the donation form author.
+ * - $sid: The unique submission ID of this submission.
+ */
+?>
+
+<div class="fundraiser-confirmation">
+  <?php if ($confirmation_message): ?>
+    <?php print $confirmation_message ?>
+  <?php else: ?>
+    <p><?php print t('Thank you, your submission has been received.'); ?></p>
+  <?php endif; ?>
+</div>
+
+<div class="links">
+  <a href="<?php print url('node/'. $node->nid) ?>"><?php print t('Go back to the form') ?></a>
+</div>
