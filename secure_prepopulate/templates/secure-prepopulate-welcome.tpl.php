316fb0c445a3f86aa799204b437ebe2fee10f0e3
Phillip Cave <phillip.cave@jacksonriver.com>
2011-04-13 11:06:45 -0400

    Add secure pre-populate module

---
+++ secure_prepopulate/templates/secure-prepopulate-welcome.tpl.php
@@ -0,0 +1,20 @@
+<?php
+// $Id: secure-prepopulate-welcome.tpl.php,v 1.0 2011/13/04 22:05:27 pcave Exp $
+
+/**
+ * @file
+ * Used to display a welcome message when a webform is accessed using an encrypted query string.
+ *
+ * This file may be renamed "secure_prepopulate_welcome_[nid].tpl.php" to target a
+ * specific webform on your site. Or you can leave it
+ * "secure_prepopulate_welcome.tpl.php" to affect all webforms on your site.
+ *
+ * Available variables:
+ * - $values: An array of key/value pairs that were passed in via query string.
+ * - $nid: The nid of the node for which the values apply.
+ */
+?>
+
+<div id="welcome-back">
+  Welcome back <? print $values['first_name']; ?>. Not <? print $values['first_name']; ?>? <? print l('Click here', 'secure-prepopulate/not-me/' . $nid); ?>
+</div>
