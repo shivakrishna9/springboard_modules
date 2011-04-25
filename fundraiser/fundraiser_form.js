7d1a600a9cde196e4586c0ad63ce779a7ecc047c
Allen Freeman <afreemannc@gmail.com>
2011-04-25 11:26:36 -0400

    Added auto-populate to label fields when an amount is entered per #147

--- fundraiser/fundraiser_form.js
+++ fundraiser/fundraiser_form.js
@@ -1,9 +1,11 @@
 Drupal.behaviors.fundraiserBehavior = function() {
+  // Auto-populate the internal name field
   $("#edit-title").blur(function() {
     $title = $(this).attr('value');
     $("#edit-internal-name").val($title);
   });
 
+  // Remove form elements when remove button is clicked.
   $("#ask-amounts .form-submit").click(function(e) {
     $(this).hide();
     $name = $(this).attr('id');
@@ -13,6 +15,14 @@ Drupal.behaviors.fundraiserBehavior = function() {
     $("#edit-label-" + $delta).remove();
     $("#edit-label-" + $delta + "-wrapper, #edit-amount-" + $delta + "-wrapper").remove();
     e.preventDefault(); // Thanks for the link Brock!
+  });
+
+  // Auto-populate label fields when amount is entered
+  $("#ask-amounts .amount_field").blur(function() {
+    $id = $(this).attr('id');
+    $id = $id.split('-');
+    $delta = $id[2];
+    $("#edit-label-" + $delta).val("$" + $(this).attr('value'));
   });
 };
 
