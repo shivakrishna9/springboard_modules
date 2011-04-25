650ceef1227f4c9b3d130e2b2e572b25cb32623c
Allen Freeman <afreemannc@gmail.com>
2011-04-25 14:22:31 -0400

    Added auto-populate to default labels per #147

--- fundraiser/fundraiser_form.js
+++ fundraiser/fundraiser_form.js
@@ -1,4 +1,5 @@
 Drupal.behaviors.fundraiserBehavior = function() {
+  $fundraiser = '';
   // Auto-populate the internal name field
   $("#edit-title").blur(function() {
     $title = $(this).attr('value');
@@ -17,12 +18,27 @@ Drupal.behaviors.fundraiserBehavior = function() {
     e.preventDefault(); // Thanks for the link Brock!
   });
 
+  $("#ask-amounts .amount_field").focus(function() {
+    
+    $amount = $(this).attr('value');
+    $id = $(this).attr('id');
+    window.fundraiser_amount = {
+      id : $id,
+      amount : $amount,
+    }
+  });
   // Auto-populate label fields when amount is entered
   $("#ask-amounts .amount_field").blur(function() {
     $id = $(this).attr('id');
+    alert ('prev value:' + window.fundraiser_amount.amount);
+    $prev_amount = $fundraiser[$id];
     $id = $id.split('-');
     $delta = $id[2];
-    $("#edit-label-" + $delta).val("$" + $(this).attr('value'));
+    $amount = $(this).attr('value');
+    $label = $("#edit-label-" + $delta).attr('value');
+    if ($label == '' || $label == "$" + window.fundraiser_amount.amount) {
+      $("#edit-label-" + $delta).val("$" + $amount);
+    }
   });
 };
 
