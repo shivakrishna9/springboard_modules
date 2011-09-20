4bc97964bc59a490edad5c5da07f917f810d5ae8
Allen Freeman <afreemannc@gmail.com>
2011-09-20 11:51:58 -0400

    added template file for Google +1 button

---
+++ springboard_social/springboard-social-gplus-link.tpl.php
@@ -0,0 +1,2 @@
+<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+<div class="g-plusone" data-size="<?php print $button_size?>" data-annotation="<?php print $annotation?>" data-href="<?php print $link_url ?> data-description="<?php print $description ?>"></div>
