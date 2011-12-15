f8150c6db34db30e15f798e49017220625bb0dab
Allen Freeman <afreemannc@gmail.com>
2011-12-15 11:16:40 -0500

    #282 - added UI for adding specific items from the perm failures queue to the export queue, as well as UI for mass adding all perm failures to the export queue

--- queue_processor/queue_processor.js
+++ queue_processor/queue_processor.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
+
   $('div.batch-item .batch-item-fields').hide();  
   $('.batch-item a.object-details').click(function(e) {
   	e.preventDefault();
     $(this).parent().find('.batch-item-fields').slideToggle('fast');
+  });
+
+  // drives select all checkbox on permanent failures report
+  $('#edit-select-all').click(function() {
+    status =  $(this).attr('checked');
+    $(':checkbox').attr('checked', status);
   });
 });
