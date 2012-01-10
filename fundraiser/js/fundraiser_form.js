b3d615a1cde5e409d6c899bb432e033a03c85917
Sarah Hood <bastlynn@gmail.com>
2012-01-10 09:28:32 -0500

    Rearrange css, js.

---
+++ fundraiser/js/fundraiser_form.js
@@ -0,0 +1,43 @@
+Drupal.behaviors.fundraiserBehavior = function() {
+  $fundraiser = '';
+  // Auto-populate the internal name field
+  $("#edit-title").blur(function() {
+    $title = $(this).attr('value');
+    $("#edit-internal-name").val($title);
+  });
+
+  // Remove form elements when remove button is clicked.
+  $("#ask-amounts .form-submit").click(function(e) {
+    $(this).hide();
+    $name = $(this).attr('id');
+    $name = $name.split('-');
+    $delta = $name[2];
+    $("#edit-amount-" + $delta).remove();
+    $("#edit-label-" + $delta).remove();
+    $("#edit-label-" + $delta + "-wrapper, #edit-amount-" + $delta + "-wrapper").remove();
+    e.preventDefault(); // Thanks for the link Brock!
+  });
+
+  $("#ask-amounts .amount_field").focus(function() {
+    
+    $amount = $(this).attr('value');
+    $id = $(this).attr('id');
+    window.fundraiser_amount = {
+      id : $id,
+      amount : $amount,
+    }
+  });
+  // Auto-populate label fields when amount is entered
+  $("#ask-amounts .amount_field").blur(function() {
+    $id = $(this).attr('id');
+    $prev_amount = $fundraiser[$id];
+    $id = $id.split('-');
+    $delta = $id[2];
+    $amount = $(this).attr('value');
+    $label = $("#edit-label-" + $delta).attr('value');
+    if ($label == '' || $label == "$" + window.fundraiser_amount.amount) {
+      $("#edit-label-" + $delta).val("$" + $amount);
+    }
+  });
+};
+
