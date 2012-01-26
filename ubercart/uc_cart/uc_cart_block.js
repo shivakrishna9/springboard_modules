5d07e9480fb1adcfcd49d5dfe3cec0b085c8090f
Sarah Hood <bastlynn@gmail.com>
2012-01-26 13:23:23 -0500

    7.x version of ubercart

--- ubercart/uc_cart/uc_cart_block.js
+++ ubercart/uc_cart/uc_cart_block.js
@@ -1,51 +1,18 @@
-
 /**
  * @file
  * Adds effects and behaviors to the cart block.
  */
 
 /**
- * Set the behavior to (un)collapse the cart block on a click
+ * Sets the behavior to (un)collapse the cart block on a click
  */
-Drupal.behaviors.ucCollapseBlock = function(context) {
-  $('.cart-block-title-bar:not(.ucCollapseBlock-processed)', context).addClass('ucCollapseBlock-processed').click(
-    function() {
-      cart_block_toggle();
-    }
-  );
-}
-
-/**
- * Collapse the shopping cart block at page load.
- */
-$(document).ready(
-  function() {
-    if (Drupal.settings.ucCollapsedBlock == true) {
-      // Add the appropriate title bar class.
-      $('.cart-block-title-bar').addClass('cart-block-toggle');
-
-      // Add the appropriate arrow class.
-      $('.cart-block-arrow').removeClass('arrow-down').addClass('arrow-up');
-    }
-    else {
-      // Add the appropriate arrow class.
-      $('.cart-block-arrow').removeClass('arrow-up').addClass('arrow-down');
-    }
-  }
-);
-
-/**
- * Toggle the shopping cart block open and closed.
- */
-function cart_block_toggle() {
-  // Toggle the display of the cart contents table.
-  $('#cart-block-contents').toggle();
-
-  // Toggle the class of the cart block arrow.
-  if ($('.cart-block-arrow').hasClass('arrow-up')) {
-    $('.cart-block-arrow').removeClass('arrow-up').addClass('arrow-down');
-  }
-  else {
-    $('.cart-block-arrow').removeClass('arrow-down').addClass('arrow-up');
+Drupal.behaviors.ucCollapseBlock = {
+  attach: function(context) {
+    jQuery('.cart-block-title-bar:not(.ucCollapseBlock-processed)', context).addClass('ucCollapseBlock-processed').click(
+      function() {
+        var $items = jQuery('.cart-block-items').toggleClass('collapsed');
+        jQuery('.cart-block-arrow').toggleClass('arrow-down', $items.hasClass('collapsed'));
+      }
+    );
   }
 }
