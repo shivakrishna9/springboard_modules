920c46e59e73b251e2792f75912b281c86666c25
Sarah Hood <bastlynn@gmail.com>
2012-06-28 15:56:27 -0400

    Correcting js throughout.

--- fundraiser/modules/fundraiser_sustainers/js/sustainer.js
+++ fundraiser/modules/fundraiser_sustainers/js/sustainer.js
@@ -2,25 +2,25 @@
 Drupal.behaviors.fundraiserSustainer = {
   attach: function(context) { (function($) {
   
-$(document).ready(function() {
-  // clear out the cancellation reason when it receives the focus for the first time
-  $('#edit-reason').one('focus', function() {
-    $(this).val('');
+  $(document).ready(function() {
+    // clear out the cancellation reason when it receives the focus for the first time
+    $('#edit-reason').one('focus', function() {
+      $(this).val('');
+    });
+    
+    /* Disable submit buttons on the 3 main forms when submitting */
+    $('#fundraiser-donation-amount-form').submit(function() {
+      $('input[type=submit]', this).attr('disabled', 'disabled');
+    });
+    
+    $('#fundraiser-billing-update-form').submit(function() {
+      $('input[type=submit]', this).attr('disabled', 'disabled');
+    });
+    
+    $('#fundraiser-cancel-form').submit(function() {
+      $('input[type=submit]', this).attr('disabled', 'disabled');
+    }); 
   });
-  
-  /* Disable submit buttons on the 3 main forms when submitting */
-  $('#fundraiser-donation-amount-form').submit(function() {
-    $('input[type=submit]', this).attr('disabled', 'disabled');
-  });
-  
-  $('#fundraiser-billing-update-form').submit(function() {
-    $('input[type=submit]', this).attr('disabled', 'disabled');
-  });
-  
-  $('#fundraiser-cancel-form').submit(function() {
-    $('input[type=submit]', this).attr('disabled', 'disabled');
-  }); 
-});
 
   })(jQuery); }
 } 
