b1e128bea40c6ab7526f87918475100763ed1846
Sarah Hood <bastlynn@gmail.com>
2012-07-03 16:52:22 -0400

    standards compliance - removing spare spaces.

--- fundraiser/modules/fundraiser_sustainers/js/sustainer.js
+++ fundraiser/modules/fundraiser_sustainers/js/sustainer.js
@@ -1,13 +1,13 @@
 /* javascript functions for sustainer management page */
 Drupal.behaviors.fundraiserSustainer = {
   attach: function(context) { (function($) {
-  
+
   $(document).ready(function() {
     // clear out the cancellation reason when it receives the focus for the first time
     $('#edit-reason').one('focus', function() {
       $(this).val('');
     });
-    
+
     /* Disable submit buttons on the 3 main forms when submitting */
     $('#fundraiser-donation-amount-form').submit(function() {
       $('input[type=submit]', this).attr('disabled', 'disabled');
