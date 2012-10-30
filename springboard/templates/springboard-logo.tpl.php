2c621bc304329c99e71d7aa2bccbf5ccfa14f6b9
Sarah Hood <bastlynn@gmail.com>
2012-10-30 03:24:05 -0400

    Completed port, adding themeable tmps.

---
+++ springboard/templates/springboard-logo.tpl.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file
+ * Customize form report for the modal output.
+ *
+ * Available variables:
+ * - $logo: The logo image.
+ */
+?>
+<div class="springboard-pane" id="springboard-logo-pane">
+  <?php print $variables['logo']; ?>
+</div>
