d2f0819cd3728c51f787443d662b2027df587acf
Phillip Cave <phillip.cave@jacksonriver.com>
2012-11-29 14:17:45 -0500

    Adding new versions of salesforce_webform and salesforce_queue.

---
+++ salesforce/salesforce_queue/salesforce_queue.api.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @file
+ * These are the hooks that are invoked by the Salesforce queue.
+ *
+ * Core hooks are typically called in all modules at once using
+ * module_invoke_all().
+ */
