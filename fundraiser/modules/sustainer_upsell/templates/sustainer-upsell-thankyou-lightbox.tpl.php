ab8f6f90bfe56c31d3401e0dc560b062029526d6
Ron Northcutt <rlnorthcutt@gmail.com>
2012-12-04 23:20:28 -0600

    Clean commit of the working sustainer_upsell module. We will be making changes to it, but this has been tested vith 3.3 and has solid basic functionality.

---
+++ fundraiser/modules/sustainer_upsell/templates/sustainer-upsell-thankyou-lightbox.tpl.php
@@ -0,0 +1,12 @@
+<div id="sustainer-upsell-thankyou-lightbox" class="<?php print $classes; ?>">
+  <div class="inner">
+    <h3>THANK YOU</h3>
+    <h2>for your support!</h2>
+    <div class="inner-content">
+      <?php print $message; ?>    
+      <?php print $match; ?>  
+    </div>
+    <?php print $content; ?>
+    <div onclick="parent.Lightbox.end();" class="modal-close">Return to site</div>
+  </div>
+</div>
---
+++ fundraiser/modules/sustainer_upsell/templates/sustainer-upsell-thickbox.tpl.php
@@ -0,0 +1,29 @@
+<div id="sustainer-upsell-thickbox" class="<?php print $classes; ?>">
+  <div class="inner">
+    <h3>Become a</h3>
+    <h2>MONTHLY DONOR</h2>
+    <div class="inner-content">
+      <?php print $message; ?>    
+      <?php print $match; ?>  
+    </div>
+    <?php print $content; ?>
+    <div class="disclaimer">
+      <p>By signing up to be a monthly donor, your credit card will be charged the monthly amount you specify beginning next month.</p>
+    </div>
+  </div>
+</div>
+<script>
+$(function() {
+  $('#edit-close').click(function() {
+    <?php
+      if(variable_get('sustainer_upsell_rejection_enabled',0) == 1):
+        ?>
+          $.cookie("sustainer_upsell_rejection", 1, { path : "/",  expires : <?php print variable_get('sustainer_upsell_rejection_lifetime',0) ?> });
+        <?php
+      endif;
+    ?>
+    window.parent.tb_remove();
+    return false;
+  });
+});
+</script>
