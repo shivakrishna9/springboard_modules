bc2368f05dc7983f0c3bccb0fbeff5384be4df03
Kathleen Murtagh <kathleen.murtagh@gmail.com>
2012-12-18 10:49:29 -0800

    Convert salesforce_queue_insert() and salesforce_queue_enqueue() to wrappers
    The main interface is the class, so these just wrap the class
    
    Also more clearly defined the hooks and alters.

--- salesforce/salesforce_queue/salesforce_queue.api.php
+++ salesforce/salesforce_queue/salesforce_queue.api.php
@@ -3,7 +3,49 @@
 /**
  * @file
  * These are the hooks that are invoked by the Salesforce queue.
+ */
+
+/**
+ * @defgroup salesforce_queue_hooks Salesforce Queue Hooks
+ * @{
+ * Salesforce Queue's hooks enable other modules to intercept events within
+ * Salesforce Queue, such as the adding an item to the queue.
+ */
+
+/**
+ * Alter an item before it is added to the queue
  *
- * Core hooks are typically called in all modules at once using
- * module_invoke_all().
+ * @param object $sObject
+ *   Salesforce sObject
+ * @param int $drupal_id
+ *   Unique identifier of the Drupal object, such as the entity ID
+ * @param string $module
+ *   Module implementing the data type
+ * @param varchar $delta
+ *   Name or id of the subtype
+ */
+function hook_salesforce_queue_create_item_alter(&$sObject, $drupal_id, $module, $delta) {
+
+}
+
+/**
+ * Fire off behavior after an item has been added to the queue
+ *
+ * @param object $sObject
+ *   Salesforce sObject
+ * @param int $drupal_id
+ *   Unique identifier of the Drupal object, such as the entity ID
+ * @param string $module
+ *   Module implementing the data type
+ * @param varchar $delta
+ *   Name or id of the subtype
+ * @param bool $result
+ *   The result of the attempt to create a new queue item
+ */
+function hook_salesforce_queue_create_item($sObject, $drupal_id, $module, $delta, $result) {
+
+}
+
+/**
+ * @}
  */
