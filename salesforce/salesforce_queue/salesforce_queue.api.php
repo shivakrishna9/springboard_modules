f16df752289048f41110d6d0bbccb7ea71c247d8
Kathleen Murtagh <kathleen.murtagh@gmail.com>
2013-01-10 17:06:44 -0800

    Coder enforcement
    Damn is it strict.  Sheesh.

--- salesforce/salesforce_queue/salesforce_queue.api.php
+++ salesforce/salesforce_queue/salesforce_queue.api.php
@@ -13,7 +13,7 @@
  */
 
 /**
- * Alter an item before it is added to the queue
+ * Alter an item before it is added to the queue.
  *
  * @param array $item
  *   Array of values to be added to the queue
@@ -23,7 +23,7 @@ function hook_salesforce_queue_create_item_alter(&$item) {
 }
 
 /**
- * Fire off behavior after an item has been added to the queue
+ * Fire off behavior after an item has been added to the queue.
  *
  * @param array $item
  *   Array of values that was added to the queue
@@ -36,7 +36,7 @@ function hook_salesforce_queue_create_item($item, $result) {
 }
 
 /**
- * Fire off behavior before an item is deleted from the queue
+ * Fire off behavior before an item is deleted from the queue.
  *
  * @param object $item
  *   The object to be deleted from the queue
@@ -46,32 +46,35 @@ function hook_salesforce_queue_delete_item($item) {
 }
 
 /**
- * Fire off behavior before the queue processing begins
+ * Fire off behavior before the queue processing begins.
  *
  * This is triggered before any verification that the process is even capable of
  * being run
  *
  * @param SalesforceQueue $queue
+ *   SalesforceQueue object
  */
 function hook_salesforce_queue_preprocess_queue(SalesforceQueue $queue) {
 
 }
 
 /**
- * Allows you to alter items retreived from the queue before they are processed
+ * Allows you to alter items retreived from the queue before they are processed.
  *
  * @param array $items
  *   Array of queue objects
  * @param SalesforceQueue $queue
+ *   SalesforceQueue object
  */
 function hook_salesforce_queue_process_items_alter(&$items, $queue) {
 
 }
 
 /**
- * Fire off behavior after the queue processing completes
+ * Fire off behavior after the queue processing completes.
  *
  * @param SalesforceQueue $queue
+ *   SalesforceQueue object
  * @param int $end_response
  *   The status of the process when it ended.  Possible values are:
  *     - SALESFORCE_QUEUE_PROCESS_NOT_STARTED: No processing has been initiated
@@ -94,44 +97,50 @@ function hook_salesforce_queue_postprocess_queue(SalesforceQueue $queue, $end_re
 }
 
 /**
- * Fire off behavior before a batch is processed
+ * Fire off behavior before a batch is processed.
  *
  * @param array $batch
  *   A batch array containing the salesforce object type, operation and items
  * @param SalesforceQueue $queue
+ *   SalesforceQueue object
  */
 function hook_salesforce_queue_preprocess_batch($batch, SalesforceQueue $queue) {
 
 }
 
 /**
- * Allows you to alter a batch before it is sent to Salesforce
+ * Allows you to alter a batch before it is sent to Salesforce.
  *
  * @param array $batch
  *   Array of instructions that are used to formulate the API call to Salesforce
  * @param SalesforceQueue $queue
+ *   SalesforceQueue object
  * @param SalesforceQueueProcessor $processor
+ *   SalesforceQueueProcessor object
  */
 function hook_salesforce_queue_process_batch_alter(&$batch, SalesforceQueue $queue, SalesforceQueueProcessor $processor) {
 
 }
 
 /**
- * Fire off behavior when an item succeeds processing
+ * Fire off behavior when an item succeeds processing.
  *
  * @param object $item
+ *   Queue item
  * @param varchar $result
  *   The Salesforce response after the call was made
  * @param SalesforceQueue $queue
+ *   SalesforceQueue object
  */
 function hook_salesforce_queue_process_pass_item($item, $result, SalesforceQueue $queue) {
 
 }
 
 /**
- * Fire off behavior when an item fails processing
+ * Fire off behavior when an item fails processing.
  *
  * @param object $item
+ *   Queue item
  * @param int $reason
  *   The status of the item when it failed processing.  Possible reasons are:
  *     - SALESFORCE_QUEUE_PROCESS_ITEM_FAIL_LEASE_EXPIRED: The item could not
@@ -150,20 +159,23 @@ function hook_salesforce_queue_process_pass_item($item, $result, SalesforceQueue
  *     - SALESFORCE_QUEUE_PROCESS_ITEM_FAIL_BATCH: The entire batch failed to be
  *         processed due to some error with Salesforce
  * @param mixed $result
+ *   Information about the result, likely a Salesforce result object
  * @param SalesforceQueue $queue
+ *   SalesforceQueue object
  */
 function hook_salesforce_queue_process_fail_item($item, $reason, $result, SalesforceQueue $queue) {
 
 }
 
 /**
- * Fire off behavior after a batch is processed
+ * Fire off behavior after a batch is processed.
  *
  * @param array $batch
  *   A batch array containing the salesforce object type, operation and items
  * @param array $results
  *   The results of the process of sending the batch to Salesforce
  * @param SalesforceQueue $queue
+ *   SalesforceQueue object
  */
 function hook_salesforce_queue_postprocess_batch($batch, $results, SalesforceQueue $queue) {
 
