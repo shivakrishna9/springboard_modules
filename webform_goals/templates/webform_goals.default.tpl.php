6f6536904cb11a81ae5277649144c510d9adce86
Allen Freeman <allen.freeman@jacksonriver.com>
2013-02-15 23:26:45 -0500

    initial dev commit of Webform Goals, not ready for primetime

---
+++ webform_goals/templates/webform_goals.default.tpl.php
@@ -0,0 +1,17 @@
+<div class='webform-goal-wrapper'>
+<span class="goal-title">[webform_goal:name]</span>
+<div class="progress-bar-wrapper">
+<div class="progress-bar">&nbsp;
+</div>
+</div>
+<style type="text/css">
+div.webform-goal-wrapper div.progress-bar-wrapper {
+  border: 1px solid #aaa;
+  height: 16px;
+}
+div.webform-goal-wrapper div.progress-bar {
+  width: [webform_goal:progress]%;
+  background-color: #888;
+  height; 16px;
+}
+</style>
