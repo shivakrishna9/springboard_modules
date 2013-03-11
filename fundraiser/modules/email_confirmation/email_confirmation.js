469b1658b2b70386eb1a401fe86ec1e15e380617
Jeremy Isett <jeremy.isett@jacksonriver.com>
2013-03-11 08:05:05 -0400

    Updating comment

--- fundraiser/modules/email_confirmation/email_confirmation.js
+++ fundraiser/modules/email_confirmation/email_confirmation.js
@@ -1,5 +1,5 @@
 Drupal.behaviors.emailBehavior = function(){
-  // We have to set the action before the preview button is clicked for ctools modal compatibilty
+  // Set the action before the preview button is clicked for ctools modal compatibilty
   $('#fundraiser-confirmation-settings-form').get(0).setAttribute('action', Drupal.settings.basePath + 'email_confirmation/preview_modal');
   // When the standard submit is clicked set it to the current location
   $('#fundraiser-confirmation-settings-form #edit-submit').click(function(){
