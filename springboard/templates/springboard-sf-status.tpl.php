352204467c478760ede262a0416ecdc443d7da9c
Sheena Donnelly <sheena.donnelly@gmail.com>
2013-03-21 16:57:45 -0400

    Springboard Dashboard - revisions to springboard connection and batch status, creating 'add form' buttons.

--- springboard/templates/springboard-sf-status.tpl.php
+++ springboard/templates/springboard-sf-status.tpl.php
@@ -11,7 +11,6 @@
 ?>
 <div class="springboard-pane" id="salesforce-status-pane">
   <p><?php print isset($variables['status']) ? t('Salesforce Connected') : t('Salesforce not connected'); ?></p>
-  <p>Last WSDL Update: <?php print isset($variables['updated_time']) ? date('Y-m-d g:i:sA', $updated_time) : t('Unknown'); ?></p>
   <p><?php print isset($variables['batch_status']) ? $variables['batch_status'] : ''; ?></p>
   <div id="springboard-sync"><span>Run Springboard Sync</span>
   <?php print l('<img src="' . base_path() . drupal_get_path('module', 'springboard') . '/images/sync.png">',
