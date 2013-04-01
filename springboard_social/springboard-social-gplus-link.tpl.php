167f1423e9d91246e2b3d22d5ced6b8742a0b7fb
Sarah Hood <bastlynn@gmail.com>
2013-04-01 19:06:36 -0400

    Archiving sp_social and moving under springboard module

--- springboard_social/springboard-social-gplus-link.tpl.php
+++
@@ -1,2 +0,0 @@
-<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
-<div class="g-plusone" data-size="<?php print $button_size?>" data-annotation="<?php print $annotation?>" data-href="<?php print $link_url ?> data-description="<?php print $description ?>"></div>
