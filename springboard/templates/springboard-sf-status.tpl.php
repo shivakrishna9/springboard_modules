c89424d53475b669ce085eb6d9a7699dd61dc613
Phillip Cave <phillip.cave@jacksonriver.com>
2013-04-10 14:52:18 -0400

    Adding SF identity information to Springboard dashboard.
    All identity info is now available in the template, but this
    commit is only outputting the connected username and SF org ID.

--- springboard/templates/springboard-sf-status.tpl.php
+++ springboard/templates/springboard-sf-status.tpl.php
@@ -7,11 +7,16 @@
  * - $status: The Salesforce connection status.
  * - $updated_time: The last time the WSDL was updated.
  * - $batch_status: The status of the latest batch.
+ * - $identity: Array of Salesforce identity information. Only populated if $status is TRUE.
  */
 ?>
 <div class="springboard-pane" id="salesforce-status-pane">
   <p><?php print $variables['status'] ? '<span class="sf-connected">' . t('Salesforce connected') . '</span>' : '<span class="sf-notconncected">' .
     t('Salesforce not connected ') . l(t('Update authentication'), 'admin/config/services/salesforce') . '</span>'; ?></p>
+  <?php if ($status): ?>
+  <p>Connected as: <?php print $identity['username'];?></p>
+  <p>Organization Id: <?php print $identity['organization_id'];?></p>
+  <?php endif; ?>
   <p><?php print isset($variables['batch_status']) ? $variables['batch_status'] : ''; ?></p>
   <p><?php print render($queue); ?></p>
 </div>
