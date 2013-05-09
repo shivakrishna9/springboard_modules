750167fa09efe91ae2ad4c719b55b466a5bc15d5
Sarah Hood <bastlynn@gmail.com>
2013-05-09 11:30:00 -0400

    Merge redundant code from fundraiser_webform into donation_validation

--- fundraiser/js/donation_validation.js
+++ fundraiser/js/donation_validation.js
@@ -15,18 +15,20 @@
       })(jQuery);
 
       $(window).ready(function(){
+        // Turn autocomplete off on CC and CVV form elements.
+        $('input[name*="card_number"], input[name*="card_cvv"]').attr('autocomplete','off');
 
         // Custom Validation Regex rules: AMEX, VISA, MASTERCARD, DISCOVER, Diner's Club, JCB
-        $.validator.addMethod('creditcard', function(value, element) { 
-          return this.optional(element) || /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/i.test(value);   
+        $.validator.addMethod('creditcard', function(value, element) {
+          return this.optional(element) || /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/i.test(value);
           // Doesn't work for Australian Bankcard, Dankort (PBS) cards or Switch/Solo (Paymentech)
           // Bankcard regexp below needs fixing
           //^5610\5[6-9]d{2}\d{4}\d{4}$
         }, "Enter a valid credit card number");
-        
+
         // Custom amount validation
         $.validator.addMethod('amount', function(value, element) {
-          // Add regexp      
+          // Add regexp
           return this.optional(element) || /^[0-9]*(\.\d{1,3})*(,\d{1,3})?$/i.test(value);
         }, "Enter a valid amount");
 
@@ -41,7 +43,7 @@
           success: function(element) {
             element.text('OK').addClass('valid').closest('.control-group').removeClass('error').addClass('success');
           }
-        });         
+        });
 
         // Check initial state of form
         $('.fundraiser-donation-form').once(function(){
@@ -61,8 +63,11 @@
           }
         });
 
+        $('input[name*="card_number"]').numeric();
+        $('input[name*="card_cvv"]').numeric();
+
          // Zipcode custom validation rule
-        $('input[name~="zip"]').rules("add", {
+        $('input[name*="zip"]').rules("add", {
           required: true,
           number: true,
           minlength:5,
@@ -72,7 +77,7 @@
           }
         });
           // CVV custom validation rule
-        $('input[name~="card_cvv"]').rules("add", {
+        $('input[name*="card_cvv"]').rules("add", {
           required: true,
           number: true,
           minlength:3,
@@ -84,44 +89,45 @@
           }
         });
         // Credit Card custom validation rule
-        $('input[name~="card_number"]').rules("add", {
+        $('input[name*="card_number"]').rules("add", {
           required: true,
           creditcard: true,
-          messages: { 
+          messages: {
             required: "This field is required",
             creditcard: "Enter a valid credit card number",
           }
         });
         // Other Amount
-        $('input[name~="other_amount"]').rules("add", {
+        $('input[name*="other_amount"]').rules("add", {
           required: {
             depends: function(element) {
               if ($('input[type="radio"][name$="[amount]"][value="other"]').is(":checked"))
                 return true;
-              else 
+              else
                 return false;
             }
-          }, 
+          },
             amount: true,
-            messages: { 
+            messages: {
             required: "This field is required",
             amount: "Enter a valid amount",
           }
         });
-                  
+
         // Focus and Blur conditional functions
-        $('input[type="radio"][name~="amount"]').change(function(){
+        $('input[type="radio"][name*="amount"]').change(function(){
           if ($(this).val() == 'other') {
-            $('input[name~="other_amount"]').focus();
+            $('input[name*="other_amount"]').focus();
           } else {
-            $('input[name~="other_amount"]').clearEle();
+            $('input[name*="other_amount"]').clearEle();
           }
         });
-        $('input[name~="other_amount"]').focus(function(){
-          $('input[type="radio"][name~="amount"][value="other"]').attr('checked', 'checked');
+        $('input[name*="other_amount"]').focus(function(){
+          $('input[type="radio"][name*="amount"][value="other"]').attr('checked', 'checked');
         })
+
         // Runs on Other Amount field
-        $('input[name~="other_amount"]').blur(function(){
+        $('input[name*="other_amount"]').blur(function(){
           var value = $(this).val();
           // Match 1-3 decimal/comma places and fix value
           if (value.match(/([\.,\-]\d{1}?)$/)) {
@@ -132,6 +138,21 @@
             $(this).val($(this).val().replace(/\D/g,''));
           }
         });
+
+        // On submission hide the button and replace it with a new value.
+        // Wrap the click in a once trigger to be sure that we bind it the one time.
+        $('.fundraiser-donation-form #edit-submit').once(function() {
+          $('.fundraiser-donation-form #edit-submit').click(function() {
+            $(this).hide();
+            $('.fundraiser_submit_message').hide();
+            $(this).after('<div class="donation-processing-wrapper">' +
+              '<p class="donation-thank-you">Thank you.</p>' +
+              '<p class="donation-processing">Your donation is being processed.</p>' +
+              '<div class="donation-processing-spinner"></div>' +
+              '</div>');
+          });
+        });
+
         // Ability to override the default message
         jQuery.extend(jQuery.validator.messages, {
           required: "This field is required",
@@ -140,7 +161,7 @@
           url: "Enter a valid URL",
           date: "Enter a valid date",
           dateISO: "Enter a valid date (ISO)",
-          number: "Must be a number", // changed 
+          number: "Must be a number", // changed
           digits: "Enter only digits",
           creditcard: "Enter a valid credit card number",
           equalTo: "Enter the same value again",
