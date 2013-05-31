0777ff02d50d5ae0b83a1a306d191d4f27858a76
Allen Freeman <allen.freeman@jacksonriver.com>
2013-05-31 13:13:30 -0400

    added checkin of Springboard Social module (under development)

---
+++ springboard_social/scripts/sb_social.addthis.js
@@ -0,0 +1,53 @@
+var addthis_config = {
+  pubid: ''
+};
+
+var addthis_share = addthis_share || {}
+addthis_share = {
+  passthrough: {
+    twitter: {
+      text: 'test'
+    }
+  }
+};
+
+(function ($) {
+  Drupal.behaviors.sb_social = {
+    attach: function (context, settings) {
+      // AddThis requires some global configuration objects be populated
+      // set up the AddThis account ID from admin settings
+      window.addthis_config = {
+        pubid: Drupal.settings.sb_social.pubid
+      };
+      // set up custom configuration for twitter message and share URL
+      window.addthis_share = {
+        url: Drupal.settings.sb_social.shortened_url,
+        passthrough: {
+          twitter: {
+            text: Drupal.settings.sb_social.twitter_message
+          }
+        }
+      };
+      // event listener logs share event
+      window.addthis.addEventListener('addthis.menu.share', shareResponse);
+    }
+  };
+
+})(jQuery);
+
+function shareResponse(data) {
+  console.log(data);
+  console.log(Drupal.settings);
+  $url = '/sb_social/share_event/' + data.data.service + '/' + Drupal.settings.sb_social.sid;
+  alert('share url:' + $url);
+  
+  // todo: fire ajax event that logs share event.
+  jQuery.ajax({
+       url: $url,
+       type: "GET",
+       success:function(response){
+         alert('share event tracker');
+         console.log(response);
+       },
+  });
+}
---
+++ springboard_social/templates/sb_social.share.tpl.php
@@ -0,0 +1,13 @@
+<div class = "addthis_toolbox addthis_default_style addthis_16x16_style">
+<?php
+if (in_array('facebook', $services)) : ?>
+<a class="addthis_button_facebook social-share-link facebook"></a>
+<?php 
+endif;
+ ?>
+<?php if (in_array('twitter', $services)) : ?>
+<a class="addthis_button_twitter social-share-link twitter"></a>
+<?php
+endif;
+?>
+</div>
