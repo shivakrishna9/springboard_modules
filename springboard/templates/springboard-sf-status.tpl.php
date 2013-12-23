6629aa12cb07f2857d6ee643011dca80778d4668
Sheena Donnelly <sheena.donnelly@jacksonriver.com>
2013-12-23 11:14:30 -0500

    re #571 - updating sync status block title.

--- springboard/templates/springboard-sf-status.tpl.php
+++ springboard/templates/springboard-sf-status.tpl.php
@@ -10,6 +10,7 @@
  * - $identity: Array of Salesforce identity information. Only populated if $status is TRUE.
  */
 ?>
+  <h2>Sync Status</h2>
   <div class="sf-status"><?php print $variables['status'] ? '<p class="sf-connected">' . t('Salesforce connected') . '</p>' : '<p class="sf-notconncected">' .
     t('Salesforce not connected ') . '</p>' . l(t('Update authentication'), 'admin/config/salesforce/authorize', array('attributes'=>array('class'=>array('button')))) . '</span>'; ?><?php print($status ? render($queue) : ''); ?></div>
   <?php if ($status): ?>
