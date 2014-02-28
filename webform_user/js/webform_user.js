d28b2ac3860a66825c3916c6958c54d1c4cf1391
klapalme <kl.jr@aol.com>
2014-02-28 10:49:02 -0500

    add check validity to triple click prevention behavior

--- webform_user/js/webform_user.js
+++ webform_user/js/webform_user.js
@@ -5,12 +5,14 @@
       $('.webform-client-form #edit-submit').once(function() {
         $('.webform-client-form #edit-submit').click(function() {
           // Disable the button. This prevents double/triple mouse clicking.
-          $(this).hide();
-          $('.fundraiser_submit_message').hide();
-          $(this).after('<div class="webform-user-wrapper">' +
-          '<p class="webform-user-thank-you">Thank you, your submission is processing.</p>' +
-          '<div class="webform-user-spinner"></div>' +
-          '</div>');
+          if($('form.webform-client-form')[0].checkValidity()) {
+            $(this).hide();
+            $('.fundraiser_submit_message').hide();
+            $(this).after('<div class="webform-user-wrapper">' +
+            '<p class="webform-user-thank-you">Thank you, your submission is processing.</p>' +
+            '<div class="webform-user-spinner"></div>' +
+            '</div>');
+          }
         });
       });
 
