76a6d77665558863cb64e00658eb9ef447af9c31
Matt Thomas <matt.thomas@jacksonriver.com>
2014-03-04 17:06:41 -0500

    re #817 - allow users to customize social share block title and description per-node

--- springboard_social/templates/sb_social.share.tpl.php
+++ springboard_social/templates/sb_social.share.tpl.php
@@ -1,3 +1,6 @@
+<div class="social_share_description">
+<?php print t($description); ?>
+</div>
 <div class = "addthis_toolbox addthis_default_style addthis_16x16_style">
 <?php print $buttons ?>
 </div>
