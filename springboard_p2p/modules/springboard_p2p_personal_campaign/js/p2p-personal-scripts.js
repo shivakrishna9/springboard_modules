64717d2854b693ab92b1f31a7cfe6ecd2c4c82e9
Danny Englander <highrockmedia@gmail.com>
2014-05-19 13:12:32 -0700

    t76 - P2P Personal campaign landing page base theme

---
+++ springboard_p2p/modules/springboard_p2p_personal_campaign/js/p2p-personal-scripts.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Misc JQuery scripts in this file
+ */
+(function ($) {
+  // Add drupal 7 code.
+  Drupal.behaviors.misc_p2pPersonal = {
+    attach: function (context, settings) {
+      // End drupal calls.
+
+
+
+      // end jquery.
+    }};
+})
+(jQuery);
