3733da762e783f90eaa1ed596a4a24e6ce21a442
Danny Englander <highrockmedia@gmail.com>
2014-05-23 14:27:06 -0700

    t17 - initial commit

---
+++ springboard_p2p/js/springboard-p2p.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Misc JQuery scripts in this file
+ */
+(function ($) {
+  // Add drupal 7 code.
+  Drupal.behaviors.p2pJS = {
+    attach: function (context, settings) {
+// End drupal calls.
+
+
+      $('#edit-mail').parent('.control-group').addClass('email-wrapper');
+
+
+      // end jquery.
+    }};
+})
+(jQuery);
