835931d1fdc9c1e751f1b8b283a7e7442a6f6219
Sheena Donnelly <sheena.donnelly@jacksonriver.com>
2014-05-30 18:35:47 -0400

    re #856 - updating Asset Library page to work more dynamically like the Forms pages to allow listing of other types of content.

---
+++ springboard/modules/springboard_admin/templates/springboard-admin-asset-page.tpl.php
@@ -0,0 +1,5 @@
+<?php foreach ($types as $type) : ?>
+   <h2><?php print $type->name; ?>s</h2>
+   <!--<a href="/springboard/add/<?php print preg_replace('/_/', '-', $type->type); ?>" class="button">Create <?php print $type->name; ?></a>-->
+   <?php print($tables[$type]); ?>
+<?php endforeach; ?>
