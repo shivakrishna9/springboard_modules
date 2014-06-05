44b34f3e027fd272253ef07cbc87c4d3bc2efc79
Danny Englander <highrockmedia@gmail.com>
2014-06-05 15:20:20 -0700

    t78 - donation page theming.

--- springboard_p2p/js/springboard-p2p.js
+++ springboard_p2p/js/springboard-p2p.js
@@ -14,6 +14,43 @@
     // Get rid of inline styles.
    $('.panel-display p').attr('style', '');
 
+/* P2P donation form */
+
+      // Add meaningful classes to webform radio buttons & labels.
+      $('input[type=checkbox]').addClass('checkbox-input');
+      $('input[type=checkbox]').next('label').addClass('checkbox-label');
+
+      $('.webform-client-form input[type="radio"]').each(function() {
+        $(this).next("label").addClass($(this).attr("type") + '-' + $(this).attr("value"));
+        $(this).addClass('radio-button-input');
+        $(this).next("label").addClass('radio-label');
+      });
+
+      // checkboxes checked / not checked.
+      $('input[type=checkbox]').click(function () {
+        if ($(this).is(':checked')) {
+          $(this).next('label').toggleClass('checked', this.checked).removeClass('not-checked');
+        }
+        else {
+          $(this).next('label').addClass('not-checked').removeClass('checked');
+        }
+      });
+
+      // Generic checked / unchecked for radio.
+      var radioChecked = $('.single-radio input').is(':checked');
+      $('.single-radio input').click(function() {
+        radioChecked = !radioChecked;
+        $(this).attr('checked', radioChecked);
+      });
+
+      // Test for radio onclick.
+      var $radioButtons = $('.webform-client-form input[type="radio"]');
+      $radioButtons.click(function() {
+        $radioButtons.each(function() {
+          $(this).next('label').toggleClass('radio-checked', this.checked);
+        });
+      });
+
 
       // end jquery.
     }};
