d9e6c6d51e036e2a8405111c5e9d123d9468cb13
Sheena Donnelly <sheena.donnelly@jacksonriver.com>
2014-06-17 10:54:30 -0400

    re #883 - removing exposed filters from donation forms, all forms, and asset library pages.

--- springboard/modules/springboard_admin/templates/springboard-admin-forms-page.tpl.php
+++ springboard/modules/springboard_admin/templates/springboard-admin-forms-page.tpl.php
@@ -2,4 +2,5 @@
 <h2><?php print $type->name; ?>s</h2>
    <a href="/node/add/<?php print preg_replace('/_/', '-', $type->type); ?>" class="button">Create <?php print $type->name; ?></a>
    <?php print($tables[$type->type]); ?>
+   <a class="button more-button" href="/admin/springboard/<?php print ($springboard_type == 'fundraiser') ? 'donation-forms' : 'forms'; ?>/<?php print $type->type; ?>"><?php print t('View all');?> <?php print $type->name; ?><?php print t('s');?> </a>
 <?php endforeach; ?>
