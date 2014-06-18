0256b64446332bc33942427cee5017c0231a25c4
Danny Englander <highrockmedia@gmail.com>
2014-06-18 11:32:31 -0700

    t840 - fixing broken buttons

--- springboard/modules/springboard_admin/templates/springboard-admin-forms-page.tpl.php
+++ springboard/modules/springboard_admin/templates/springboard-admin-forms-page.tpl.php
@@ -1,6 +1,7 @@
 <?php foreach ($types as $type) : ?>
 <h2><?php print $type->name; ?>s</h2>
-   <a href="/node/add/<?php print preg_replace('/_/', '-', $type->type); ?>" class="button">Create <?php print $type->name; ?></a>
+   <a href="/node/add/<?php print preg_replace('/_/', '-', $type->type); ?>" class="button button-create button-left">Create <?php print $type->name; ?></a>
    <?php print($tables[$type->type]); ?>
-   <a class="button more-button" href="/admin/springboard/<?php print ($springboard_type == 'fundraiser') ? 'donation-forms' : 'forms'; ?>/<?php print $type->type; ?>"><?php print t('View all');?> <?php print $type->name; ?><?php print t('s');?> </a>
+   <a class="button more-button" href="/admin/springboard/<?php print ($springboard_type == 'fundraiser') ? 'donation-forms' : 'forms'; ?>/<?php print $type->type; ?>">
+     <?php print t('View all');?> <?php print $type->name; ?><?php print t('s');?> </a>
 <?php endforeach; ?>
