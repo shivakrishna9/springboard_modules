51a67aa774ce2b06afab25a1347a74da21a3ec74
Danny Englander <highrockmedia@gmail.com>
2014-07-23 14:26:45 -0700

    t1,2,3 - adding title inline with boday, added form top template

--- form_layouts/templates/two-column-webform-form-right.tpl.php
+++ form_layouts/templates/two-column-webform-form-right.tpl.php
@@ -9,6 +9,7 @@
 <div id="donation-form-column-wrapper" class="form-layout-container">
   <div class="form-row">
     <div id="left" class="form-col">
+      <h1 class="form-layout-title"><?php print render($element['title']); ?></h1>
       <?php print render($element['body']); ?>
     </div>
     <div id="right" class="form-col">
--- form_layouts/templates/two-column-webform-form-top.tpl.php
+++ form_layouts/templates/two-column-webform-form-top.tpl.php
@@ -8,10 +8,10 @@
 
 <div id="donation-form-column-wrapper" class="form-layout-container">
   <div class="form-row">
-    <div id="left" class="form-col">
+    <div id="left" class="form-col-row top">
       <?php print render($element['webform']); ?>
     </div>
-    <div id="right" class="form-col">
+    <div id="right" class="form-col-row below">
       <h1 class="form-layout-title"><?php print render($element['title']); ?></h1>
       <?php print render($element['body']); ?>
     </div>
