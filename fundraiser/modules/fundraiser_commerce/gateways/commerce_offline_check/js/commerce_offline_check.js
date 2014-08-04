688d358abf97bdf4aa2fb2717c2754a9edc146f3
klapalme <kl.jr@aol.com>
2014-08-04 12:52:24 -0400

    prevent states.js from making offline check fields required

--- fundraiser/modules/fundraiser_commerce/gateways/commerce_offline_check/js/commerce_offline_check.js
+++ fundraiser/modules/fundraiser_commerce/gateways/commerce_offline_check/js/commerce_offline_check.js
@@ -3,9 +3,12 @@
     attach: function(context) {
       $('textarea[name$="[offline_check][message]"]').siblings('label').children('span.form-required').remove();
       $('input[name$="[offline_check][check_number]"]').siblings('label').children('span.form-required').remove();
-
+      $(document).bind('state:required', function(e) {
+        if (e.target.id.indexOf('offline-check') != -1) {
+          $('textarea[name$="[offline_check][message]"]').siblings('label').children('span.form-required').remove();
+          $('input[name$="[offline_check][check_number]"]').siblings('label').children('span.form-required').remove();
+        }
+      });
     }
   };
 })(jQuery);
-
-
