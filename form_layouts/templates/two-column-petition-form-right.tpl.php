a805fbbd74f8c417b5ca0ef299e476376a05bc2e
Phillip Cave <phillip.cave@jacksonriver.com>
2014-08-13 12:58:05 -0400

    Adding quick sign form and recent signers list as example.

--- form_layouts/templates/two-column-petition-form-right.tpl.php
+++ form_layouts/templates/two-column-petition-form-right.tpl.php
@@ -11,9 +11,13 @@
     <div id="left" class="form-col">
       <h1 class="form-layout-title"><?php print render($element['title']); ?></h1>
       <?php print render($element['body']); ?>
+      <?php print views_embed_view('recent_petiton_signers'); ?>
     </div>
     <div id="right" class="form-col">
       <?php print render($element['webform']); ?>
+      <div id="petition-quick-sign">
+        <?php print render($element['springboard_petition_quicksign']); ?>
+      </div>
     </div>
   </div>
 </div>
