07ee0728f8f6f23cb0e397460d4c352d4af78806
Danny Englander <highrockmedia@gmail.com>
2014-08-14 11:01:11 -0700

    t2 - themning form left

--- form_layouts/templates/two-column-petition-form-right.tpl.php
+++ form_layouts/templates/two-column-petition-form-right.tpl.php
@@ -12,7 +12,8 @@
     <div id="left" class="form-col">
       <h1 class="form-layout-title"><?php print render($element['title']); ?></h1>
       <?php print render($element['body']); ?>
-      <?php $view = views_get_view('recent_petiton_signers'); $view->set_display('block_1'); ?>
+      <?php $view = views_get_view('recent_petiton_signers');
+      $view->set_display('block_1'); ?>
       <h2><?php print $view->get_title(); ?></h2>
       <?php print $view->preview('block_1'); ?>
     </div>
@@ -20,13 +21,13 @@
     <div id="right" class="form-col">
       <div id="webform-form" class="form-wrapper">
         <div class="form-wrapper-inner">
-      <?php print render($element['webform']); ?>
-          </div>
+          <?php print render($element['webform']); ?>
+        </div>
       </div>
       <div id="petition-quick-sign" class="form-wrapper">
         <div class="form-wrapper-inner">
-        <?php print render($element['springboard_petition_quicksign']); ?>
-          </div>
+          <?php print render($element['springboard_petition_quicksign']); ?>
+        </div>
       </div>
     </div>
   </div>
