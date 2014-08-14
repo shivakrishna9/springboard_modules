e42a1e4580a1bf4a22bfa2468acc3cb275ea62d4
Danny Englander <highrockmedia@gmail.com>
2014-08-14 09:59:07 -0700

    t1 - theming form layouts

--- form_layouts/templates/two-column-petition-form-right.tpl.php
+++ form_layouts/templates/two-column-petition-form-right.tpl.php
@@ -8,17 +8,25 @@
 
 <div id="webform-form-column-wrapper" class="form-layout-container form-right">
   <div class="form-row">
+
     <div id="left" class="form-col">
       <h1 class="form-layout-title"><?php print render($element['title']); ?></h1>
       <?php print render($element['body']); ?>
-      <?php $view = views_get_view('recent_petiton_signers'); $view->set_display('signers_block'); ?>
+      <?php $view = views_get_view('recent_petiton_signers'); $view->set_display('block_1'); ?>
       <h2><?php print $view->get_title(); ?></h2>
-      <?php print $view->preview('signers_block'); ?>
+      <?php print $view->preview('block_1'); ?>
     </div>
+
     <div id="right" class="form-col">
+      <div id="webform-form" class="form-wrapper">
+        <div class="form-wrapper-inner">
       <?php print render($element['webform']); ?>
-      <div id="petition-quick-sign">
+          </div>
+      </div>
+      <div id="petition-quick-sign" class="form-wrapper">
+        <div class="form-wrapper-inner">
         <?php print render($element['springboard_petition_quicksign']); ?>
+          </div>
       </div>
     </div>
   </div>
