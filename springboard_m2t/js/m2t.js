49446e40d78f5414baae6b3305d599bb61e5d0b7
klapalme <kl.jr@aol.com>
2014-08-21 13:42:17 -0400

    add ajax functionality and sunlight lookup

--- springboard_m2t/js/m2t.js
+++ springboard_m2t/js/m2t.js
@@ -2,19 +2,64 @@
   Drupal.behaviors.m2t = {
     attach: function (context, settings) {
 
-    $("form.geocode-form").submit(function() {
+      var addressFields = 'input[name$="address]"], input[name*="city"], select[name*="state"], input[name*="zip"]';
       var address = '';
-      var geocoder = new google.maps.Geocoder();
-      geocoder.geocode({'address': address}, function (results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        var latitude = results[0].geometry.location.lat();
-        var longitude = results[0].geometry.location.lng();
-      } else {
-        //something
+
+      $('form.geocode-form').on('blur', addressFields, function(){
+        parseAddresFields();
+      });
+
+      $('form.geocode-form').on('change', 'select[name*="state"]', function(){
+        parseAddresFields();
+      });
+
+      function parseAddresFields() {
+        $(addressFields,'form.geocode-form').each(function(){
+           if(this.value === '') {
+            return false;
+           }
+           else {
+            address += this.value + ', ';
+           }
+           if(this.name == 'submitted[sbp_zip]' && this.value !== '') {
+             lookupGeo();
+           }
+        });
       }
-    });
-    return false;
-  });
-  }
-};
+
+      function lookupGeo() {
+        var geocoder = new google.maps.Geocoder();
+        geocoder.geocode({'address': address}, function (results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            var latitude = results[0].geometry.location.lat();
+            var longitude = results[0].geometry.location.lng();
+            console.log(latitude + ' ' + longitude);
+
+
+            $.ajax({
+              type: "POST",
+              url: '/sunlightlookup',
+              data: {coords: [latitude, longitude]},
+              dataType: 'json',
+              success: function(data) {
+                console.log(data);
+              },
+
+              error: function(xhr, textStatus, error){
+                console.log(error);
+              }
+            });
+
+
+
+
+          } else {
+            //something
+          }
+        });
+        return false;
+      }
+
+    }
+  };
 })(jQuery);
