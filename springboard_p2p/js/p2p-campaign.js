d7dc76a52b8e0da2a7f6902b9220428685ad2243
David Barbarisi <davidbarbarisi@gmail.com>
2014-08-26 10:51:35 -0400

    changed hide/show to disabled/enabled for suggested donation amount.

--- springboard_p2p/js/p2p-campaign.js
+++ springboard_p2p/js/p2p-campaign.js
@@ -9,14 +9,14 @@
             $('.springboard_p2p_campaign_goal_table_fundraiser input[type=checkbox]', context).change(function(e) {
                 var thisCheck = $(this);
                 if (thisCheck.is(':checked')) {
-                    $('#node_p2p_campaign_form_group_p2p_donation_amount', context).show();
+                    $('#node_p2p_campaign_form_group_p2p_donation_amount input', context).prop('disabled', false);
                 }
             });
 
             $('.springboard_p2p_campaign_goal_table_webform_user input[type=checkbox]', context).change(function(e) {
                 var thisCheck = $(this);
                 if (thisCheck.is(':checked')) {
-                    $('#node_p2p_campaign_form_group_p2p_donation_amount', context).hide();
+                    $('#node_p2p_campaign_form_group_p2p_donation_amount input', context).prop('disabled', true);
                 }
             });
         }
