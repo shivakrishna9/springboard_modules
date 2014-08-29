aced081f81525191354c513475c7a49c93ceae9c
klapalme <kl.jr@aol.com>
2014-08-29 14:05:30 -0400

    add smarty streets config

--- springboard_m2t/js/m2t.js
+++ springboard_m2t/js/m2t.js
@@ -30,13 +30,13 @@
       function lookupGeo(address) {
         console.log(address);
         activeGeocoder = Drupal.settings.springboard_m2t.geocoder;
-        if (activeGeocoder == 'google') {
+        if (activeGeocoder == 'googlej') {
           var geocoder = new google.maps.Geocoder();
           geocoder.geocode({'address': address}, function (results, status) {
             if (status == google.maps.GeocoderStatus.OK) {
               var latitude = results[0].geometry.location.lat();
               var longitude = results[0].geometry.location.lng();
-              console.log(latitude + ' ' + longitude);
+              console.log(results[0]);
 
               $.ajax({
                 type: "POST",
