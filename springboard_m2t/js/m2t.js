8a254815e28ef074c9a87fe54b49dfee84307346
klapalme <kl.jr@aol.com>
2014-09-02 11:52:00 -0400

    use smarty streets api for lookup

--- springboard_m2t/js/m2t.js
+++ springboard_m2t/js/m2t.js
@@ -13,42 +13,50 @@
       });
 
       function parseAddressFields() {
-        var address = '';
+        var addressObj = {};
         $(addressFields,'form.geocode-form').each(function(){
            if(this.value === '') {
             return false;
            }
            else {
-            address += this.value + ', ';
+            addressObj[this.name] = this.value;
            }
            if(this.name == 'submitted[sbp_zip]' && this.value !== '')  {
-             lookupGeo(address);
+             lookupGeo(addressObj);
            }
         });
       }
 
-      function lookupGeo(address) {
+      function lookupGeo(addressObj) {
         activeGeocoder = Drupal.settings.springboard_m2t.geocoder;
-        if (activeGeocoder == 'googlej') {
-          var geocoder = new google.maps.Geocoder();
-          geocoder.geocode({'address': address}, function (results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-              var latitude = results[0].geometry.location.lat();
-              var longitude = results[0].geometry.location.lng();
-              var url =  '/sunlightlookup';
-              data = {coords: [latitude, longitude]};
-            }
+        var url = '';
+        var address = '';
+        $(addressObj).each(function(){
+            address += this.value + ', ';
           });
+        if (activeGeocoder == 'googlej') {
+          // var geocoder = new google.maps.Geocoder();
+
+          // geocoder.geocode({'address': address}, function (results, status) {
+          //   if (status == google.maps.GeocoderStatus.OK) {
+          //     var latitude = results[0].geometry.location.lat();
+          //     var longitude = results[0].geometry.location.lng();
+          //     url =  '/sunlightlookup';
+          //     data = {coords: [latitude, longitude]};
+          //   }
+          // });
         }
         else {
-          data = {onelineaddress: address};
-          if (activeGeocoder == 'census') {
-            url = '/censuslookup';
+          if (activeGeocoder == 'smartystreets_rest') {
+            data = {address: addressObj};
+            console.log(data);
+            url = '/smartylookup';
           }
-          else if (activeGeocoder == 'geocodio') {
-            url = '/geocodiolookup';
-            console.log(address);
-          }
+          // else if (activeGeocoder == 'geocodio') {
+          //   data = {onelineaddress: address};
+          //   url = '/geocodiolookup';
+          //   console.log(address);
+          // }
         }
         $.ajax({
           type: "POST",
@@ -58,7 +66,6 @@
           success: function(data) {
             console.log(data);
           },
-
           error: function(xhr, textStatus, error){
             console.log(error);
           }
