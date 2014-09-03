95c8d25c7212867648c6b079a7ec027dc3916d6a
klapalme <kl.jr@aol.com>
2014-09-03 11:05:31 -0400

    add jquery validation

--- springboard_m2t/js/m2t.js
+++ springboard_m2t/js/m2t.js
@@ -53,6 +53,78 @@
           }
         });
       }
+
+
+      (function($){
+        $.fn.clearEle = function() {
+          return this.each(function() {
+            $(this).removeClass('valid');
+            $(this).next('label').remove();
+            $(this).parents('.success').removeClass('success');
+            $(this).parents('.error').removeClass('error');
+            this.value = '';
+          });
+        };
+      })(jQuery);
+      $(window).ready(function(){
+        // Custom zipcode validation
+        $.validator.addMethod('zipcode', function(value, element) {
+          // U.S. or Canadian Zip codes
+          return this.optional(element) || /(^\d{5}((-|\s)\d{4})?$)|(^[ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1} *\d{1}[A-Z]{1}\d{1}$)/i.test(value);
+        }, "Enter a valid zipcode");
+
+        jQuery.validator.addMethod("phoneUS", function(phone_number, element) {
+          phone_number = phone_number.replace(/\s+/g, "");
+          return this.optional(element) || phone_number.length > 9 && phone_number.match(/^(\+?1-?)?(\([2-9]\d{2}\)|[2-9]\d{2})-?[2-9]\d{2}-?\d{4}$/);
+        }, "Please specify a valid phone number");
+
+        // Instantiate Form Validation
+        var m2tValidate = $('.geocode-form').validate({
+          // Custom keyup function checking for tab key (9) and when value is empty
+          onkeyup: function (element, event) {
+            if ($(element).next('.error')[0]){
+              if (event.which === 9 && element.value === "") {
+                return;
+              } else {
+                this.element(element);
+              }
+            }
+          },
+          onfocusout: function (element) {
+            // Callback for real-time onfocusout of form elements
+            var isValid = $(element).valid();
+            //
+            if (typeof validateKeyCallback != "undefined" && isValid === 0) {
+              // Set status to 0
+              window.validateKeyCallback.status = 0;
+              validateKeyCallback.error(element);
+            } else if (typeof validateKeyCallback != "undefined" && isValid == 1) {
+              // Set status to 1
+              window.validateKeyCallback.status = 1;
+              validateKeyCallback.success(element);
+            }
+          },
+          highlight: function(element) {
+            $(element).addClass('key-validate');
+            $(element).closest('.control-group').removeClass('success').addClass('error');
+          },
+          success: function(element) {
+            $(element).text('OK').addClass('valid').closest('.control-group').removeClass('error').addClass('success');
+          }
+        });
+        $('input[name*="zip"]').rules("add", {
+          required: true,
+          zipcode: true,
+        });
+        $('input[name*="phone"]').rules("add", {
+          required: true,
+          phoneUS: true,
+        });
+        $('input[name*="mail"]').rules("add", {
+          required: true,
+          email: true,
+        });
+      });
     }
   };
 })(jQuery);
