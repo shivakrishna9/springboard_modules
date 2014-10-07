908c8ae48b517eaf56de125ab1ba0b34ae2304b6
David Barbarisi <davidbarbarisi@gmail.com>
2014-10-07 17:17:35 -0400

    broke out the billing update processing so it could be used for Account Updater.

--- fundraiser/modules/fundraiser_commerce/gateways/commerce_litle/includes/CommerceLitleAccountUpdater.php
+++ fundraiser/modules/fundraiser_commerce/gateways/commerce_litle/includes/CommerceLitleAccountUpdater.php
@@ -38,9 +38,9 @@ class CommerceLitleAccountUpdater {
    * Add a month to the given end date.
    *
    * @param int $year
-   *  The year.
+   *   The year.
    * @param int $month
-   *  The month.
+   *   The month.
    *
    * @return array
    *   An array with year and month keys.
@@ -60,81 +60,80 @@ class CommerceLitleAccountUpdater {
    *
    * @param DOMDocument $response
    *   The XML response document.
-   *
    * @param object $order
    *   The order object that has just been charged.
    *   This will be used as the template to create additional donations
    *   if needed.
+   *
+   * @return bool
+   *   TRUE if an account update was detected and attempted.
+   *   FALSE if nothing changed or we don't have enough info
+   *   to perform the update.
    */
   public function processResponseXML(DOMDocument $response, $order) {
     $list = $response->getElementsByTagName('accountUpdater');
 
     if ($list->length == 0) {
-      return;
+      // No Account Updater tags.
+      return FALSE;
     }
 
     $data = $this->getDataFromXML($list->item(0));
 
     // Do some error checking.
     if (!isset($data['originalCardTokenInfo']) || !isset($data['newCardTokenInfo'])) {
-      return;
+      return FALSE;
     }
 
-    $old_exp_date = $this->convertExpDateString($data['originalCardTokenInfo']['expDate']);
+    $changed = $this->cardHasChanged($data['originalCardTokenInfo'], $data['newCardTokenInfo']);
 
-    $card = $this->findMatchingCardonfile($data['originalCardTokenInfo']['litleToken'], $old_exp_date);
+    if ($changed) {
+      // Load the card on file card.
+      $old_exp_date = $this->convertExpDateString($data['originalCardTokenInfo']['expDate']);
+      $card = $this->findMatchingCardonfile($data['originalCardTokenInfo']['litleToken'], $old_exp_date, $order->uid);
 
-    if (is_null($card)) {
-      // No card with a matching remote id or exp date.
-      // Bail for now.
-      // @todo Do something? Create a new cardonfile?
-      return;
-    }
+      if (is_null($card)) {
+        // No card with a matching remote id or exp date or uid.
+        // Do nothing even though we have new data.
+        return FALSE;
+      }
 
-    $changed = FALSE;
-
-    // If the Litle Account Updater returns a new token,
-    // we save that new token with the series/scheduled payments.
-    // A new token implies a new last 4 digits of the card number.
-    if ($data['originalCardTokenInfo']['litleToken'] != $data['newCardTokenInfo']['litleToken']) {
-
+      // Save the new token info to the card on file.
+      // A new token implies a new last 4 digits of the card number.
       $new_token = $data['newCardTokenInfo']['litleToken'];
       $new_number = substr($new_token, -4);
       $new_type = _commerce_litle_litle_card_type_to_cc($data['newCardTokenInfo']['type']);
 
       $card->remote_id = $new_token;
       // The long form of the card_type.
-      $card->type = $new_type;
+      $card->card_type = $new_type;
       // The card_number, which is the last 4 of the token for credit cards.
       $card->card_number = $new_number;
 
-      $changed = TRUE;
-    }
-
-    // If the Litle Account Updater returns a new expiration date,
-    // we create recurring donations through that new expiration date (+1 month,
-    // see #703 for reference).
-    if ($data['originalCardTokenInfo']['expDate'] != $data['newCardTokenInfo']['expDate']) {
-      // Call create future orders with new exp date.
-      // Edit card_exp_month and card_exp_year.
+      // Convert the date format Litle uses to the one we use, and save it.
       $new_exp_date = $this->convertExpDateString($data['newCardTokenInfo']['expDate']);
       $card->card_exp_month = $new_exp_date['month'];
       $card->card_exp_year = $new_exp_date['year'];
 
+      $card->changed = REQUEST_TIME;
+
+      // Update billing info on existing future donations.
+      // and create recurring donations through the new expiration date.
       $donation = fundraiser_donation_get_donation($order->order_id);
       if (isset($donation->recurring->master_did)) {
         $master_donation = fundraiser_donation_get_donation($donation->recurring->master_did);
-        $this->createFutureOrders($master_donation, $new_exp_date['month'], $new_exp_date['year'], $donation);
+        $submission_fields = $this->formatSubmissionFields($new_number, $new_type, $new_exp_date);
+
+        // Only save the card on file if we are able to update the billing info
+        // as well.
+        commerce_cardonfile_save($card);
+        $this->updateBillingInfo($master_donation, $submission_fields);
+
+        return TRUE;
       }
-
-      $changed = TRUE;
     }
 
-    if ($changed) {
-      $card->changed = REQUEST_TIME;
-      commerce_cardonfile_save($card);
-    }
-
+    return FALSE;
   }
 
   /**
@@ -186,36 +185,114 @@ class CommerceLitleAccountUpdater {
 
 
   /**
-   * Get a matching cardonfile for the given token and expiration date.
+   * Get a matching cardonfile for the given token, expiration date, and uid.
    *
    * @param string $remote_id
    *   The Litle token, usually from litleToken.
    * @param array $exp_date
    *   An array with month and year keys.
+   * @param int $uid
+   *   User ID for the card on file.
    *
-   * @return null|object
-   *   The card entity, or NULL if nothing's found.
+   * @return bool|object
+   *   The card entity, or FALSE if nothing's found.
    */
-  public function findMatchingCardonfile($remote_id, array $exp_date) {
-    $card = NULL;
-    $cards = commerce_cardonfile_load_multiple_by_remote_id($remote_id);
-    // Could be an array or FALSE.
-    if (!$cards || empty($cards)) {
-      return $card;
+  public function findMatchingCardonfile($remote_id, array $exp_date, $uid) {
+    $query = new EntityFieldQuery();
+    $query->entityCondition('entity_type', 'commerce_cardonfile')
+      ->propertyCondition('remote_id', $remote_id)
+      ->propertyCondition('uid', $uid)
+      ->propertyCondition('card_exp_year', $exp_date['year'])
+      ->propertyCondition('card_exp_month', $exp_date['month']);
+
+    $result = $query->execute();
+
+    if (isset($result['commerce_cardonfile'])) {
+      $cards = commerce_cardonfile_load_multiple(array_keys($result['commerce_cardonfile']));
+    }
+    else {
+      return FALSE;
     }
 
-    foreach ($cards as $potential_card) {
-      // Check the card on file agains the old expiration date.
-      if ($potential_card->card_exp_month == $exp_date['month'] && $potential_card->card_exp_year == $exp_date['year']) {
-        $card = $potential_card;
-        break;
-      }
+    // Could be an array or FALSE.
+    if (!$cards || empty($cards)) {
+      return FALSE;
     }
+
+    $card = reset($cards);
 
     return $card;
   }
 
-  protected function createFutureOrders($donation, $month, $year, $source_donation) {
-    _fundraiser_sustainers_create_future_orders($donation, $month, $year, $source_donation, NULL);
+  /**
+   * Formats an array for $submission_fields.
+   *
+   * @param int $card_number
+   *   Last four digits of the card number.
+   * @param string $card_type
+   *   The card type.
+   * @param array $exp_date
+   *   Has month and year keys.
+   * @param string $payement_method
+   *   The payment method key to use.
+   *
+   * @return array
+   *   An array that can be merged into $donation->donation to update payment.
+   */
+  protected function formatSubmissionFields($card_number, $card_type, array $exp_date, $payement_method = 'credit') {
+    // To be merged into $donation->donation.
+    return array(
+      'payment_fields' => array(
+        $payement_method => array(
+          'card_number' => $card_number,
+          'card_cvv' => '',
+          'card_type' => $card_type,
+          'card_expiration_month' => $exp_date['month'],
+          'card_expiration_year' => $exp_date['year'],
+        ),
+      ),
+    );
+  }
+
+  /**
+   * Update the billing info on future sustainers and extend the series.
+   *
+   * @param object $master_donation
+   *   The master donation for the sustainer series.
+   * @param array $submission_fields
+   *   The fields to merge into $donation->donation so they will get saved.
+   */
+  protected function updateBillingInfo($master_donation, array $submission_fields) {
+    $name = 'Litle Account Updater';
+    $donations = _fundraiser_sustainers_get_donations_recurr_remaining($master_donation->did);
+
+    // Update billing info in the currently existing donations.
+    fundraiser_sustainers_update_billing_info_in_series($donations, $submission_fields, $name);
+
+    // Use the first future donation for a template of new future donations.
+    // By this point it has the new billing info.
+    $source_donation = reset($donations);
+    $source_donation = fundraiser_donation_get_donation($source_donation->did);
+
+    // Create new donations if needed.
+    fundraiser_sustainers_update_billing_info_create_new_donations($master_donation, $source_donation, $submission_fields);
+
+  }
+
+  /**
+   * Compare the original tokenized card data with the new data.
+   *
+   * @param array $original
+   *   An array of original data with litleToken and expDate keys.
+   * @param array $new
+   *   An array of new data with litleToken and expDate keys.
+   *
+   * @return bool
+   *   TRUE if this is a different card.
+   */
+  protected function cardHasChanged($original, $new) {
+    return
+      $original['litleToken'] != $new['litleToken']
+      || $original['expDate'] != $new['expDate'];
   }
 }
