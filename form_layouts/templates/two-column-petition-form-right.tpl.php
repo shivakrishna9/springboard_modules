ba070a343b848578eb5282782512340dd6fd0fa9
Allen Freeman <allen.freeman@jacksonriver.com>
2014-10-08 14:36:43 -0400

    Form layout templates for petitions now only conditionally show recent signers block title if there are signatures.

--- form_layouts/templates/two-column-petition-form-right.tpl.php
+++ form_layouts/templates/two-column-petition-form-right.tpl.php
@@ -13,8 +13,13 @@
       <h1 class="form-layout-title"><?php print render($element['title']); ?></h1>
       <?php print render($element['body']); ?>
       <?php $view = views_get_view('recent_petiton_signers');
-      $view->set_display('block_1'); ?>
+      $view->set_display('block_1');
+      $view->set_arguments(array(arg(1)));
+      $view->pre_execute();
+      $view->execute();?>
+      <?php if (isset($view->total_rows) && $view->total_rows): ?>
       <h2><?php print $view->get_title(); ?></h2>
+      <?php endif; ?>
       <?php print $view->preview('block_1'); ?>
     </div>
 
