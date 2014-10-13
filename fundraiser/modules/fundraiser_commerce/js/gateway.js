9ab8aa007bf57beb8da07777eec47e1c93fc6ad7
Phillip Cave <phillip.cave@jacksonriver.com>
2014-10-13 16:13:42 -0400

    Slight adjustment to message.

--- fundraiser/modules/fundraiser_commerce/js/gateway.js
+++ fundraiser/modules/fundraiser_commerce/js/gateway.js
@@ -2,7 +2,7 @@ Drupal.behaviors.fundraiserGateway = {
   attach: function(context) { (function($) {
     $(document).ready(function() {
       $("#edit-submit").click(function(){
-        return confirm("A misconfiguration can cause transactions to fail. Are you sure you want to proceed?");
+        return confirm("Please review your changes carefully. A misconfiguration can cause transactions to fail.\n\nAre you sure you want to proceed?");
       });
     });
   })(jQuery);}
