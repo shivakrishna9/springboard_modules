4d62cb542e779048aa7458f0416540e5fb399460
Phillip Cave <phillip.cave@jacksonriver.com>
2014-10-15 15:29:32 -0400

    Updating templates to grab updated recent signatures list view.

--- form_layouts/templates/two-column-petition-form-right.tpl.php
+++ form_layouts/templates/two-column-petition-form-right.tpl.php
@@ -12,7 +12,7 @@
     <div id="left" class="form-col">
       <h1 class="form-layout-title"><?php print render($element['title']); ?></h1>
       <?php print render($element['body']); ?>
-      <?php $view = views_get_view('recent_petition_signers');
+      <?php $view = views_get_view('recent_petition_signatures');
       $view->set_display('block_1');
       $view->set_arguments(array(arg(1)));
       $view->pre_execute();
