c7ee0390f8703ab82822425a3f06b5ef6fe0e1a4
David Barbarisi <davidbarbarisi@gmail.com>
2014-11-14 12:48:46 -0500

    Changed the Views controller for fundraiser sustainer records so gateway response will allow empty filtering.

---
+++ fundraiser/modules/fundraiser_sustainers/includes/FundraiserSustainerRecordViewsController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @file
+ * The Views controller class for the Fundraiser Sustainer record entity.
+ */
+
+class FundraiserSustainerRecordViewsController extends EntityDefaultViewsController {
+
+  /**
+   * Defines the result for hook_views_data().
+   *
+   * For the Fundraiser Sustainer record entity. This allows filtering on
+   * gateway response using "is null" and "is not null".
+   *
+   * @return array
+   *   Data for Views.
+   */
+  public function views_data() {
+    $data = parent::views_data();
+    $table = $this->info['base table'];
+    $data[$table]['gateway_resp']['filter']['allow empty'] = TRUE;
+    return $data;
+  }
+}
