db0fbb4d37a085aad4124d6660b39f01c6d3649d
David Barbarisi <davidbarbarisi@gmail.com>
2014-11-17 19:36:03 -0500

    the report link text is supposed to be translated.

--- fundraiser/modules/fundraiser_sustainers/includes/FundraiserSustainersHealthChecks.php
+++ fundraiser/modules/fundraiser_sustainers/includes/FundraiserSustainersHealthChecks.php
@@ -47,7 +47,7 @@ class FundraiserSustainersHealthChecks {
         'message' => 'The following sustainer donations are locked but not processed: %dids',
         'variables' => array('%dids' => implode(', ', $stuck_sustainers)),
         'severity' => WATCHDOG_CRITICAL,
-        'link' => l('Report', 'admin/springboard/reports/reconciliation/locked-not-processed-sustainers'),
+        'link' => l(t('Report'), 'admin/springboard/reports/reconciliation/locked-not-processed-sustainers'),
       );
       $this->issues[] = $issue;
     }
