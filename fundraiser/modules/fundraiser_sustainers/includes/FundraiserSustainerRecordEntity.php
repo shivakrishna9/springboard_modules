8c4222987c06d8b2aef7defa98b8397c1c8fd7dd
David Barbarisi <davidbarbarisi@gmail.com>
2014-12-22 15:42:15 -0500

    Removed revisonable entity related files.

--- fundraiser/modules/fundraiser_sustainers/includes/FundraiserSustainerRecordEntity.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class FundraiserSustainerRecordEntity extends Entity {
-
-  public function __construct(array $values = array(), $entityType = NULL) {
-    parent::__construct($values, 'fundraiser_sustainer_record');
-  }
-}
