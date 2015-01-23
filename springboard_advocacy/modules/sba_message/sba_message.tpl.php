1a5d35ebb85db8452d0e15bd50b1a37e76192e35
klapalme <kl.jr@aol.com>
2015-01-23 15:18:26 -0500

    update comments in sba_message .tpl

--- springboard_advocacy/modules/sba_message/sba_message.tpl.php
+++ springboard_advocacy/modules/sba_message/sba_message.tpl.php
@@ -2,22 +2,22 @@
 
 /**
  * @file
- * A basic template for model entities
+ * A basic template for sba_message entities
  *
  * Available variables:
  * - $content: An array of comment items. Use render($content) to print them all, or
  *   print a subset such as render($content['field_example']). Use
  *   hide($content['field_example']) to temporarily suppress the printing of a
  *   given element.
- * - $title: The name of the model
- * - $url: The standard URL for viewing a model entity
+ * - $title: The name of the sba_message
+ * - $url: The standard URL for viewing a sba_message entity
  * - $page: TRUE if this is the main view page $url points too.
  * - $classes: String of classes that can be used to style contextually through
  *   CSS. It can be manipulated through the variable $classes_array from
  *   preprocess functions. By default the following classes are available, where
  *   the parts enclosed by {} are replaced by the appropriate values:
  *   - entity-profile
- *   - model-{TYPE}
+ *   - sba_message-{TYPE}
  *
  * Other variables:
  * - $classes_array: Array of html class attribute values. It is flattened
