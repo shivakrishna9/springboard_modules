8584c7eb6d7247eaba0b3de5263333fa717343c3
klapalme <kl.jr@aol.com>
2015-01-29 09:47:44 -0500

    add manage link to sba views template

--- springboard_advocacy/templates/springboard-advocacy-dashboard.tpl.php
+++ springboard_advocacy/templates/springboard-advocacy-dashboard.tpl.php
@@ -7,9 +7,14 @@
     <?php if (isset($view['header'])): ?>
       <h2 class="types"><?php print $view['header'];?></h2>
     <?php endif; ?>
+    <div class="buttons-wrapper">
+    <?php if (isset($view['manage_link'])): ?>
+      <div class="buttons-wrapper"><?php print l($view['manage_link']['title'], $view['manage_link']['href'], array('attributes' => array('class' => array('button', 'more-button')))); ?></div>
+    <?php endif; ?>
     <?php if (isset($view['create_link'])): ?>
       <div class="buttons-wrapper"><?php print l($view['create_link']['title'], $view['create_link']['href'], array('attributes' => array('class' => array('button', 'add-button')))); ?></div>
     <?php endif; ?>
+   </div>
     <?php if (isset($view['content'])): ?>
       <?php print $view['content']; ?>
       <?php if (isset($view['viewmore_link'])): print l($view['viewmore_link']['title'], $view['viewmore_link']['href'], array('attributes' => array('class' => array('button', 'more-button')))); endif; ?>
