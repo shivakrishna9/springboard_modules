010bd89766b09fe9fb8bc055f146aa453b763ccc
danny-englander <highrockmedia@gmail.com>
2015-02-04 08:53:35 -0800

    t243 - theming insights page

--- fundraiser/modules/fundraiser_sustainers/templates/fundraiser_sustainers_insights_dashboard.tpl.php
+++ fundraiser/modules/fundraiser_sustainers/templates/fundraiser_sustainers_insights_dashboard.tpl.php
@@ -1,31 +1,32 @@
 <h2>Today's Snapshot <?php print $today ?></h2>
 
-<div><?php print $time_range_links; ?></div>
+<div class="time-range-links"><?php print $time_range_links; ?></div>
 
 <div class="row">
-  <div class="insights-today-box span3">
+  <div class="insights-today-box span3 icharges">
     <h3>Scheduled Charges</h3>
     <div class="big-metric"><?php print $scheduled_charges ?></div>
     <div class="foot"><?php print l('View scheduled', $today_scheduled_path); ?></div>
   </div>
 
-  <div class="insights-today-box span3">
-    <h3>Total Value</h3>
-    <div class="big-metric"><?php print $total_value ?></div>
-    <div class="foot"></div>
-  </div>
-
-  <div class="insights-today-box span3">
+  <div class="insights-today-box span3 isuccesses">
     <h3>Successes</h3>
     <div class="big-metric"><?php print $successes ?></div>
     <div class="foot"><?php print l('View successes', $today_success_path); ?></div>
   </div>
 
-  <div class="insights-today-box span3">
+  <div class="insights-today-box span3 ifailures">
     <h3>Failures</h3>
     <div class="big-metric"><?php print $failures ?></div>
     <div class="foot"><?php print l('View failures', $today_failure_path); ?></div>
   </div>
+
+  <div class="insights-today-box span3 itotal-value">
+    <h3>Total Value</h3>
+    <div class="big-metric"><?php print $total_value ?></div>
+    <div class="foot"></div>
+  </div>
+
 </div>
 <hr>
 
