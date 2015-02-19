c69c5146fc3a1cc24159a2f63de93fe8361ff223
klapalme <kl.jr@aol.com>
2015-02-19 16:04:10 -0500

    add css option to advocacy dashboard theme

--- springboard_advocacy/templates/springboard-advocacy-dashboard.tpl.php
+++ springboard_advocacy/templates/springboard-advocacy-dashboard.tpl.php
@@ -1,18 +1,22 @@
 <?php 
   /** @TODO documentation here **/
+
 ?>
 
 <?php foreach ($views as $view): ?>
+  <?php if (isset($view['css'])):
+    drupal_add_css($view['css']); ?>
+  <?php endif; ?>
   <div class="types-wrapper p2p-views">
     <?php if (isset($view['header'])): ?>
       <h2 class="types"><?php print $view['header'];?></h2>
     <?php endif; ?>
     <div class="buttons-wrapper">
     <?php if (isset($view['manage_link'])): ?>
-      <div class="buttons-wrapper"><?php print l($view['manage_link']['title'], $view['manage_link']['href'], array('attributes' => array('class' => array('button', 'more-button')))); ?></div>
+      <div class="sba-button"><?php print l($view['manage_link']['title'], $view['manage_link']['href'], array('attributes' => array('class' => array('button', 'more-button')))); ?></div>
     <?php endif; ?>
     <?php if (isset($view['create_link'])): ?>
-      <div class="buttons-wrapper"><?php print l($view['create_link']['title'], $view['create_link']['href'], array('attributes' => array('class' => array('button', 'add-button')))); ?></div>
+      <div class="sba-button"><?php print l($view['create_link']['title'], $view['create_link']['href'], array('attributes' => array('class' => array('button', 'sba-add-button')))); ?></div>
     <?php endif; ?>
    </div>
     <?php if (isset($view['content'])): ?>
