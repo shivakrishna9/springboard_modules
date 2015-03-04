223b4199d5a478e0a7e0d1e0373360f09a9b5dde
klapalme <kl.jr@aol.com>
2015-03-04 11:23:47 -0500

    add message action image field

--- springboard_advocacy/modules/sba_message_action/templates/form_layouts/two-column-message-right.tpl.php
+++ springboard_advocacy/modules/sba_message_action/templates/form_layouts/two-column-message-right.tpl.php
@@ -10,13 +10,19 @@
 
 <div id="message-action-form-column-wrapper" class="container-fluid">
   <div class="row-fluid">
+    <div id="call-to-action" class="span8">
+      <?php print $message_call_to_action; ?>
+    </div>
+    <div id="image" class="span4">
+      <?php print $message_image; ?>
+    </div>
+  </div>
+  <div class="row-fluid">
     <div id="left" class="span6">
       <?php print drupal_render_children($form); ?>
     </div>
     <div id="right" class="span6">
       <?php print $message_fieldset; ?> d
     </div>
-  </div>
-  <div class="row-fluid">
   </div>
 </div>
