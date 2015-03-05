8050164d1298b6e0e6dcb03666400f792a45590f
klapalme <kl.jr@aol.com>
2015-03-05 14:10:26 -0500

    sba_message_action form layouts

--- springboard_advocacy/modules/sba_message_action/templates/form_layouts/two-column-message-right.tpl.php
+++ springboard_advocacy/modules/sba_message_action/templates/form_layouts/two-column-message-right.tpl.php
@@ -8,7 +8,12 @@
 
 <?php print drupal_render($form['submitted']); ?>
 
-<div id="message-action-form-column-wrapper" class="container-fluid">
+<div id="message-action-form-2col-right-column-wrapper" class="container-fluid">
+  <div class="row-fluid">
+    <div id="goals" class="span12">
+      <div class="wg-goal" style="display: none;" data-gid="1"></div>
+    </div>
+  </div>
   <div class="row-fluid">
     <div id="call-to-action" class="span8">
       <?php print $message_call_to_action; ?>
@@ -17,9 +22,11 @@
       <?php print $message_image; ?>
     </div>
   </div>
+  <h3>Take action now!</h3>
   <div class="row-fluid">
     <div id="left" class="span6">
       <?php print drupal_render_children($form); ?>
+     <p><?php print check_markup('[sb_social:facebook_share_link][sb_social:twitter_share_link][sb_social:email_share_link]', 'full_html'); ?></p>
     </div>
     <div id="right" class="span6">
       <?php print $message_fieldset; ?>
