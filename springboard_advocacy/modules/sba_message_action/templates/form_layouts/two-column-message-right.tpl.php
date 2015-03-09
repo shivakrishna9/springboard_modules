14423f9e4afb8d375fad24bc9cf88ab68777a39e
klapalme <kl.jr@aol.com>
2015-03-09 16:21:05 -0400

    sba_message_action non-form-layouts theme

--- springboard_advocacy/modules/sba_message_action/templates/form_layouts/two-column-message-right.tpl.php
+++ springboard_advocacy/modules/sba_message_action/templates/form_layouts/two-column-message-right.tpl.php
@@ -4,22 +4,27 @@
  * This renders a message action form in 2 columns.
  */
 ?>
-<?php //print drupal_render($form) ?>
 
 
 <div id="message-action-form-2col-right-column-wrapper" class="container-fluid">
-<!--  <div class="row-fluid">-->
-<!--    <div id="call-to-action" class="span8">-->
-<!--      --><?php //print $message_call_to_action; ?>
-<!--    </div>-->
-<!--    <div id="image" class="span4">-->
-<!--      --><?php //print $message_image; ?>
-<!--    </div>-->
-<!--  </div>-->
-  <h3>Take action now!</h3>
+  <div class="row-fluid">
+    <div id="call-to-action" class="span8">
+      <?php print $body; ?>
+    </div>
+    <div id="image" class="span4">
+      <?php print $image; ?>
+    </div>
+  </div>
+  <fieldset id="take-action-wrapper" class = "form-wrapper"><legend><?php print $form_legend ?></legend>
+    <div class = "fieldset-wrapper">
   <div class="row-fluid">
     <div id="left" class="span5">
-      <p><?php print $webform ?></p>
+      <fieldset id="take-action-wrapper" class = "form-wrapper"><legend><?php print $user_legend ?></legend>
+        <div class = "fieldset-wrapper">
+
+        <p><?php print $webform ?></p>
+        </div>
+      </fieldset>
       <?php print drupal_render_children($form); ?>
 
       <p><?php print $social ?></p>
@@ -27,6 +32,7 @@
     <div id="right" class="span7">
       <?php print $message_fieldset; ?>
     </div>
+    </div>
   </div>
-
+</fieldset>
 </div>
