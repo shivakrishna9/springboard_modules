6b0050494f6d647a394417f18b4f65cb8dd4d39e
klapalme <kl.jr@aol.com>
2015-03-12 09:25:58 -0400

    dynamic resize sba message textarea

--- springboard_advocacy/modules/sba_message_action/js/sba_message_action.js
+++ springboard_advocacy/modules/sba_message_action/js/sba_message_action.js
@@ -1,17 +1,31 @@
 /**
  * @file
- * Handles Views' exposed form AJAX data submission.
- * see https://www.drupal.org/node/1183418
+ * message textarea sizing.
  */
 (function ($) {
 
     $(document).ready(function () {
+        setColumnHeight();
+        var sizer;
+        $(window).resize(function(){
+            clearTimeout(sizer);
+            sizer = setTimeout(function() {
+                setColumnHeight();
+            }, 200);
+        })
+    });
+
+    function setColumnHeight(){
         var formHeight = $('#user-information-wrapper').height();
         var textColumn = $('textarea[id*="edit-sba-messages-message"]').parents('.message-column');
         var formColumn = $('#user-information-wrapper').parents('.message-column');
         if (textColumn.attr('id') != formColumn.attr('id')) {
             var fHeight = formColumn.height();
             var tHeight = textColumn.height();
+            if (formColumn.css('float') == 'none') {
+                $('textarea[id*="edit-sba-messages-message"]').css('min-height', '230px');
+                return false;
+            }
             if (formHeight > tHeight) {
                 var areaHeight = $('textarea[id*="edit-sba-messages-message"]').height();
                 var plus = fHeight - tHeight;
@@ -19,7 +33,5 @@
                 $('textarea[id*="edit-sba-messages-message"]').css('min-height', newHeight);
             }
         }
-
-  });
-
+    }
 })(jQuery);
