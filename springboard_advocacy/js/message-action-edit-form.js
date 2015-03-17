898ecbd96fbd694b561309fb8b4fae6d1d6e035f
Joel Rothschild <joel.rothschild@jacksonriver.com>
2015-03-17 16:07:01 -0400

    Added "Alert options" fieldset (as vertical tab) to Message Action edit form, with columnized versions of the taxonomy select fields for Congressional and Organizational Issue fields.

---
+++ springboard_advocacy/js/message-action-edit-form.js
@@ -0,0 +1,66 @@
+/**
+ * @file
+ * Handles Views' exposed form AJAX data submission.
+ * see https://www.drupal.org/node/1183418
+ */
+(function ($) {
+
+    /*
+     * Gets Form build info from settings and adds it to ajax data.
+     *
+     * @see views_exposed_form_ajax_enable().
+     */
+    Drupal.behaviors.AdvocacyMessageActionEditForm = {
+        attach: function(context, settings) {
+            // Columnize the Congressional Issues list based on the average label width.
+            var labelWidthsTotal = 0;
+            var labelCount = 0;
+            $('#edit-springboard-advocacy-options .field-name-field-sba-congressional-issues .form-type-checkbox').each(function(){
+                labelWidthsTotal += $(this).find('label').outerWidth();
+                labelCount++;
+            });
+            var checkboxWidth =
+                $('#edit-springboard-advocacy-options .field-name-field-sba-congressional-issues .form-type-checkbox input')
+                    .outerWidth()
+            ;
+            if (labelCount > 0) {
+                var averageLabelWidth = labelWidthsTotal / labelCount;
+                var bufferWidth = averageLabelWidth * 1.2;  // Add 20% buffer.
+                var columnWidth = Math.ceil(averageLabelWidth + checkboxWidth + bufferWidth);
+                $('#edit-springboard-advocacy-options .field-name-field-sba-congressional-issues .form-type-checkbox')
+                    .parent().columnize({width: columnWidth})
+                ;
+            }
+
+            // Columnize the Organization Issues list
+            labelWidthsTotal = labelCount = 0;
+            $('#edit-springboard-advocacy-options .field-name-field-sba-organization-issues .form-type-checkbox').each(function(){
+                labelWidthsTotal += $(this).find('label').outerWidth();
+                labelCount++;
+            });
+            if (labelCount > 0) {
+                // When a top-level item is changed, update all of its descendants to match.
+                var onChange = function(e) {
+                    var isChecked = $(this).parent().find('input:not(.sba-term-indent)').get(0).checked;
+                    $(this).parents('.sba-term-group').find('input.sba-term-indent').attr('checked', isChecked);
+                }
+                // Wrap groups and attach click behavior.
+                var jqContainer = $('#edit-springboard-advocacy-options .field-name-field-sba-organization-issues .form-type-checkbox').parent();
+                $('#edit-springboard-advocacy-options .field-name-field-sba-organization-issues .form-type-checkbox').each(function(){
+                    if (!$(this).find('input').hasClass('sba-term-indent')) {
+                        $(this).find('input').change(onChange);
+                        // Create a new group for this top-level term.
+                        jqContainer.append('<div class="sba-term-group dontsplit"></div>');
+                    }
+                    $(this).appendTo(jqContainer.find('div.sba-term-group:last'));
+                });
+                averageLabelWidth = labelWidthsTotal / labelCount;
+                bufferWidth = averageLabelWidth * 1.3;
+                columnWidth = Math.ceil(averageLabelWidth + checkboxWidth + bufferWidth);
+                jqContainer.columnize({width: columnWidth});
+            }
+
+        }
+    };
+
+})(jQuery);
