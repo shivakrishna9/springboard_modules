66dbc613995f7407e13b13e19d54a0527e45731f
Joel Rothschild <joel.rothschild@jacksonriver.com>
2015-04-20 15:09:03 -0400

    Removed comments from message-action-edit-form.js that weren't relevant.

--- springboard_advocacy/js/message-action-edit-form.js
+++ springboard_advocacy/js/message-action-edit-form.js
@@ -1,15 +1,5 @@
-/**
- * @file
- * Handles Views' exposed form AJAX data submission.
- * see https://www.drupal.org/node/1183418
- */
 (function ($) {
 
-    /*
-     * Gets Form build info from settings and adds it to ajax data.
-     *
-     * @see views_exposed_form_ajax_enable().
-     */
     Drupal.behaviors.AdvocacyMessageActionEditForm = {
         attach: function(context, settings) {
             // Columnize the Legislative Issues list based on the average label width.
