f312f971d9287afed97f323b26b84b38be96b091
Jeremy Isett <epicflux@hotmail.com>
2015-05-19 15:57:34 -0400

    Clear the cancel reason message if it hasn't been edited.

--- fundraiser/modules/fundraiser_sustainers/js/sustainer.js
+++ fundraiser/modules/fundraiser_sustainers/js/sustainer.js
@@ -1,26 +1,32 @@
-/* javascript functions for sustainer management page */
+/**
+ * @file
+ * Javascript functions for sustainer management page.
+ */
 Drupal.behaviors.fundraiserSustainer = {
   attach: function(context) { (function($) {
 
-  $(document).ready(function() {
-    // clear out the cancellation reason when it receives the focus for the first time
+    /* Disable submit buttons on the 3 main forms when submitting */
+    $('#fundraiser-sustainers-donation-amount-form').submit(function() {
+      $('input[type=submit]', this).attr('disabled', 'disabled');
+    });
+
+    $('#fundraiser-sustainers-billing-update-form').submit(function() {
+      $('input[type=submit]', this).attr('disabled', 'disabled');
+    });
+
+    $('#fundraiser-sustainers-cancel-form').submit(function() {
+      $('input[type=submit]', this).attr('disabled', 'disabled');
+    });
+
+    // clear out the cancellation reason when it receives the focus for the first time.
     $('#edit-reason').one('focus', function() {
       $(this).val('');
     });
 
-    /* Disable submit buttons on the 3 main forms when submitting */
-    $('#fundraiser-donation-amount-form').submit(function() {
-      $('input[type=submit]', this).attr('disabled', 'disabled');
+    // Trigger the focus so it will clear out the message if it hasn't been edited.
+    $('#fundraiser-sustainers-cancel-form input[type=submit]').click(function() {
+      $('#edit-reason').trigger('focus');
     });
-
-    $('#fundraiser-billing-update-form').submit(function() {
-      $('input[type=submit]', this).attr('disabled', 'disabled');
-    });
-
-    $('#fundraiser-cancel-form').submit(function() {
-      $('input[type=submit]', this).attr('disabled', 'disabled');
-    });
-  });
 
   })(jQuery); }
 }
