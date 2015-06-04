5f2afb8c576ba764a517cc2595a6aae845b9be77
klapalme <kl.jr@aol.com>
2015-06-04 10:22:19 -0400

    advocacy code style and comments

--- springboard_advocacy/templates/springboard-advocacy-dashboard.tpl.php
+++ springboard_advocacy/templates/springboard-advocacy-dashboard.tpl.php
@@ -1,3 +1,13 @@
+<?php
+/**
+ * @file
+ * Theme the advocacy dashboard views.
+ *
+ * Available variables:
+ * - $sections: an array containing the page elements and content related to
+ * each view.
+ */
+?>
 <?php foreach ($sections as $section): ?>
   <?php if (isset($section['css'])):
     drupal_add_css($section['css']); ?>
@@ -10,13 +20,13 @@
       <h2 class="types"><?php print $section['header'];?></h2>
     <?php endif; ?>
     <div class="buttons-wrapper">
-    <?php if (isset($section['manage_link'])): ?>
-      <div class="sba-button"><?php print l($section['manage_link']['title'], $section['manage_link']['href'], array('attributes' => array('class' => array('button', 'more-button')))); ?></div>
-    <?php endif; ?>
-    <?php if (isset($section['create_link'])): ?>
-      <div class="sba-button"><?php print l($section['create_link']['title'], $section['create_link']['href'], array('attributes' => array('class' => array('button', 'sba-add-button')))); ?></div>
-    <?php endif; ?>
-   </div>
+      <?php if (isset($section['manage_link'])): ?>
+        <div class="sba-button"><?php print l($section['manage_link']['title'], $section['manage_link']['href'], array('attributes' => array('class' => array('button', 'more-button')))); ?></div>
+      <?php endif; ?>
+      <?php if (isset($section['create_link'])): ?>
+        <div class="sba-button"><?php print l($section['create_link']['title'], $section['create_link']['href'], array('attributes' => array('class' => array('button', 'sba-add-button')))); ?></div>
+      <?php endif; ?>
+    </div>
     <?php if (isset($section['content'])): ?>
       <?php print $section['content']; ?>
       <?php if (isset($section['viewmore_link'])): print l($section['viewmore_link']['title'], $section['viewmore_link']['href'], array('attributes' => array('class' => array('button', 'more-button')))); endif; ?>
