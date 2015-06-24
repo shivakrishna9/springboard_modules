c485d48eb27fa730ec87c725a3b4c302866eaca7
Cameron Kingzett <cameron.kingzett@jacksonriver.com>
2015-06-24 16:19:47 -0700

    Updated deliverability reporting to utilize theming more to make the template output overridable.

--- springboard_advocacy/modules/sba_message_action/templates/sba_message_action_deliverability.tpl.php
+++ springboard_advocacy/modules/sba_message_action/templates/sba_message_action_deliverability.tpl.php
@@ -1,31 +1,33 @@
 <h2>Overall Deliverability</h2>
+
 <div class="container deliverability-performance-container">
   <div class="row">
     <div class="span3">
       <div class="well">
-        <h2 class="text-info"><?php echo $report->data->totalMessages; ?></h2>
+        <h2 class="text-info"><?php print $report->data->totalMessages; ?></h2>
         Total Messages
       </div>
     </div>
     <div class="span3">
       <div class="well">
-        <h2 class="text-info"><?php echo $report->data->delivered; ?></h2>
+        <h2 class="text-info"><?php print $report->data->delivered; ?></h2>
         Delivered
       </div>
     </div>
     <div class="span3">
       <div class="well">
-        <h2 class="text-info"><?php echo $report->data->failed; ?></h2>
+        <h2 class="text-info"><?php print $report->data->failed; ?></h2>
         Failed
       </div>
     </div>
     <div class="span3">
       <div class="well">
-        <h2 class="text-info"><?php echo $report->data->deliverabilityIndexPercentage; ?></h2>
+        <h2 class="text-info"><?php print $report->data->deliverabilityIndexPercentage; ?></h2>
         Deliverablity
       </div>
     </div>
   </div>
 </div>
 <h2>Individual Target Deliverability</h2>
-<?php echo theme('table', array('header' => $header, 'rows' => $targets)); ?>
+
+<?php print $targets_table; ?>
