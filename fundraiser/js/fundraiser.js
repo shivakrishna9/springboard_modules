156dd3ba655045de21144f87802dfcff33c9a270
Dan Ruscoe <dan.ruscoe@jacksonriver.com>
2015-09-08 14:24:45 -0700

    Code style fixes for the donation form ask amount JS.

--- fundraiser/js/fundraiser.js
+++ fundraiser/js/fundraiser.js
@@ -5,7 +5,7 @@ Drupal.behaviors.fundraiserBehavior = {
   $("#edit-title").blur(function() {
     var title = $(this).attr('value');
     var currently = $("#edit-internal-name").attr('value');
-    if (currently.length == 0) {
+    if (currently.length === 0) {
       $("#edit-internal-name").val(title);
     }
   });
@@ -14,7 +14,7 @@ Drupal.behaviors.fundraiserBehavior = {
   $('#ask-amounts input[id$="amount"]').blur(function() {
     var amount = $(this).val();
     var label = $(this).parents('tr').find('input[id*="label"]').val();
-    if (label == '') {
+    if ((label.length === 0) && (amount.length !== 0)) {
       $(this).parents('tr').find('input[id*="label"]').val("$" + amount);
     }
   });
