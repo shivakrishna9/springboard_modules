6869ceb0a34605a9f9343a8b4d3b35448df286ff
klapalme <kl.jr@aol.com>
2015-09-29 13:49:26 -0400

    add click once protection to multistep preview form

--- springboard_advocacy/modules/sba_message_action/js/sba_message_preview.js
+++ springboard_advocacy/modules/sba_message_action/js/sba_message_preview.js
@@ -1,4 +1,31 @@
 (function ($) {
+    Drupal.behaviors.multstepForm = {
+        attach: function (context, settings) {
+            $('#sba-message-action-preview-form').find('#edit-submit').once(function() {
+                $('#sba-message-action-preview-form').find('#edit-submit').click(function() {
+                    var  button = $(this);
+                    // Disable the button. This prevents double/triple mouse clicking.
+                    if($('#sba-message-action-preview-form')[0].checkValidity) {
+                        if($('#sba-message-action-preview-form')[0].checkValidity()) {
+                            $(this).hide();
+                            $(this).after('<div class="webform-user-wrapper">' +
+                                '<p class="webform-user-thank-you">Thank you, your submission is processing.</p>' +
+                                '<div class="webform-user-spinner"></div>' +
+                                '</div>');
+                        }
+                    }
+                    else {
+                        button.hide();
+                        button.after('<div class="webform-user-wrapper">' +
+                            '<p class="webform-user-thank-you">Thank you, your submission is processing.</p>' +
+                            '<div class="webform-user-spinner"></div>' +
+                            '</div>');
+                    }
+                });
+            });
+        } // attach.function
+    } // drupal.behaviors
+
     $(document).ready(function () {
         $('textarea').each(function (e) {
             $(this).height(this.scrollHeight)
