bb7f04a3676736a315839c47d6a6d4263c48bc27
klapalme <kl.jr@aol.com>
2015-10-05 11:15:07 -0400

    alternate message preview header text and javascript submit function for send now link

--- springboard_advocacy/modules/sba_message_action/js/sba_message_preview.js
+++ springboard_advocacy/modules/sba_message_action/js/sba_message_preview.js
@@ -1,28 +1,23 @@
 (function ($) {
     Drupal.behaviors.multstepForm = {
         attach: function (context, settings) {
-            $('#sba-message-action-preview-form').find('#edit-submit').once(function() {
-                $('#sba-message-action-preview-form').find('#edit-submit').click(function() {
-                    var  button = $(this);
-                    // Disable the button. This prevents double/triple mouse clicking.
-                    if($('#sba-message-action-preview-form')[0].checkValidity) {
-                        if($('#sba-message-action-preview-form')[0].checkValidity()) {
-                            $(this).hide();
-                            $(this).after('<div class="webform-user-wrapper">' +
-                                '<p class="webform-user-thank-you">Thank you, your submission is processing.</p>' +
-                                '<div class="webform-user-spinner"></div>' +
-                                '</div>');
-                        }
-                    }
-                    else {
-                        button.hide();
-                        button.after('<div class="webform-user-wrapper">' +
-                            '<p class="webform-user-thank-you">Thank you, your submission is processing.</p>' +
-                            '<div class="webform-user-spinner"></div>' +
-                            '</div>');
-                    }
+            var editSubmit = $('#sba-message-action-preview-form').find('#edit-submit');
+            var send = $('#multiflow-send-now');
+
+            editSubmit.once(function() {
+                editSubmit.click(function() {
+                    submitOnce(editSubmit, send, this);
                 });
             });
+
+            send.once(function() {
+                send.click(function() {
+                    submitOnce(editSubmit, send, this);
+                    $('#sba-message-action-preview-form').submit();
+                    return false;
+                });
+            });
+
         } // attach.function
     } // drupal.behaviors
 
@@ -31,4 +26,38 @@
             $(this).height(this.scrollHeight)
         });
     });
+
+    function submitOnce(editSubmit, send, clicked) {
+
+        if($('#sba-message-action-preview-form')[0].checkValidity) {
+            if($('#sba-message-action-preview-form')[0].checkValidity()) {
+                editSubmit.hide();
+                var link = send.replaceWith('<span id="multiflow-send-now-processed">send now</span>');
+                if (clicked.id == 'multiflow-send-now') {
+                    rotate();
+                }
+                editSubmit.after('<div class="webform-user-wrapper">' +
+                    '<p class="webform-user-thank-you">Thank you, your submission is processing.</p>' +
+                    '<div class="webform-user-spinner"></div>' +
+                    '</div>');
+            }
+        }
+        else {
+            editSubmit.hide();
+            var link = send.replaceWith('<span id="multiflow-send-now-processed">send now</span>');
+            if (clicked.id == 'multiflow-send-now') {
+                rotate();
+            }
+            editSubmit.after('<div class="webform-user-wrapper">' +
+                '<p class="webform-user-thank-you">Thank you, your submission is processing.</p>' +
+                '<div class="webform-user-spinner"></div>' +
+                '</div>');
+        }
+    }
+    function rotate(num) {
+        num = typeof num !== 'undefined' ? num : 10;
+        $('#multiflow-send-now-wrapper').css({ 'transform': 'rotate(' + num + 'deg)'});
+        setTimeout(function() { rotate(num+10); }, 10);
+    }
+
 })(jQuery);
