7f9600747379eb3d42395a21b3f0abf430605041
Dan Ruscoe <dan.ruscoe@jacksonriver.com>
2015-10-23 16:27:13 -0400

    Added hook to alter data warehouse donation data.

---
+++ fundraiser/modules/fundraiser_data_warehousing/fundraiser_data_warehousing.api.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file
+ * Hooks for Fundraiser Data Housing.
+ */
+
+ /**
+  * Alters the donation data before it is sent to the data warehouse.
+  *
+  * @param array $donation_data
+  *   Array of donation data.
+  */
+ function hook_fundraiser_data_housing_donation_data_alter($donation_data) {
+
+ }
