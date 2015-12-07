ac75142f5dcffa28577ba227070864e493f6c896
Dan Ruscoe <dan.ruscoe@jacksonriver.com>
2015-12-07 13:16:44 -0800

    Switched token set ID reference to data property on form element.

--- springboard_token/modules/springboard_token_set/js/springboard_token_set.js
+++ springboard_token/modules/springboard_token_set/js/springboard_token_set.js
@@ -1,7 +1,22 @@
 (function ($) {
   Drupal.behaviors.SpringboardTokenSet = {
     attach: function (context, settings) {
+      $(".form-wrapper").each(function() {
+        var token_set_id = $(this).data("token-set-id");
 
+        if (typeof token_set_id !== typeof undefined) {
+          $(this).click(function() {
+            ShowTokens($(this));
+          });
+        }
+      });
+
+      function ShowTokens(element) {
+        var token_set_id = element.data("token-set-id");
+
+        console.log("ShowTokens: " + token_set_id);
+
+      }
     }
   };
 })(jQuery);
