25c47080ebe6b06cabbbef31834131a630b4e930
Dan Ruscoe <dan.ruscoe@jacksonriver.com>
2015-12-16 23:18:48 -0800

    Refactored token sets to store complete token strings.

--- springboard_token/modules/springboard_token_set/js/springboard_token_set.js
+++ springboard_token/modules/springboard_token_set/js/springboard_token_set.js
@@ -29,7 +29,7 @@
         // Generate tokens div.
         var html = '<ul id="token-set-tokens">';
         for (var i = 0; i < tokens.length; i++) {
-          html += renderToken(tokens[i].token_type, tokens[i].token_name);
+          html += renderToken(tokens[i].token);
         }
         html += '</ul>';
 
@@ -39,36 +39,33 @@
           $(this).click(function(event) {
             event.preventDefault();
 
-            var token_type = $(this).data("token-type");
-            var token_name = $(this).data("token-name");
+            var token = $(this).data("token");
 
-            insertToken(Drupal.settings.token_set_last_selected_field, token_type, token_name);
+            insertToken(Drupal.settings.token_set_last_selected_field, token);
           });
         });
       };
 
-      var renderToken = function (token_type, token_name) {
-        var html = '<li><a class="token-set-token-link" data-token-type="' + token_type + '" data-token-name="' + token_name + '" href="#">[' + token_type + ':' + token_name + ']</a></li>';
+      var renderToken = function (token) {
+        var html = '<li><a class="token-set-token-link" data-token="' + token + '" href="#">' + token + '</a></li>';
 
         return html;
       };
 
-      var insertToken = function (element, token_type, token_name) {
-        var token_string = '[' + token_type + ':' + token_name + ']';
-
+      var insertToken = function (element, token) {
         // IE support.
         if (document.selection) {
           element.focus();
           sel = document.selection.createRange();
-          sel.text = token_string;
+          sel.text = token;
         }
         // Mozilla support.
         else if (element.selectionStart || (element.selectionStart === '0')) {
           element.value = element.value.substring(0, element.selectionStart)
-            + token_string
+            + token
             + element.value.substring(element.selectionEnd, element.value.length);
         } else {
-          element.value += token_string;
+          element.value += token;
         }
       };
     }
