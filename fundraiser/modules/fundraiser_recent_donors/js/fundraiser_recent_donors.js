1891493b0ca1e611281ed316c5a0cd48d8af1e30
Josh Estep <josh.estep@jacksonriver.com>
2016-01-08 10:33:39 -0600

    Minor tweaks to fundraiser_recent_donors.js.

--- fundraiser/modules/fundraiser_recent_donors/js/fundraiser_recent_donors.js
+++ fundraiser/modules/fundraiser_recent_donors/js/fundraiser_recent_donors.js
@@ -1,9 +1,15 @@
 /**
- * Position currency suffix/prefix inline with the "Only show donations greater than" field.
+ * @file
+ * js for Fundraiser Recent Donors
  */
+
 Drupal.behaviors.fundraiserRecentDonorsBehavior = {
   attach: function(context) { (function($) {
+    // Position the opt-in checkbox near the submit button:
+
+    // Position currency suffix/prefix inline with the "Only show donations greater than" field:
     $('.form-item-show-recent-donation-greater-than input#edit-show-recent-donation-greater-than').after($('span.recent-donors-currency-symbol-after'));
     $('.form-item-show-recent-donation-greater-than input#edit-show-recent-donation-greater-than').before($('span.recent-donors-currency-symbol-before'));
   })(jQuery); }
 }
+
