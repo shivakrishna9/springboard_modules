9554b873de78e485372c101cc1b86306c0c838a1
Josh Estep <josh.estep@jacksonriver.com>
2016-02-26 14:02:33 -0500

    Adding final schema changes, jquery and css changes for the token UI.

--- springboard_token/modules/springboard_token_set/js/springboard_token_set.js
+++ springboard_token/modules/springboard_token_set/js/springboard_token_set.js
@@ -2,15 +2,19 @@
   Drupal.behaviors.SpringboardTokenSet = {
     attach: function (context, settings) {
       // Add token click handler to form elements with token set IDs.
-      $(".form-wrapper").each(function() {
+      $(".form-wrapper, #webform-component-edit-form #edit-value, #webform-component-edit-form #edit-extra-description").each(function() {
         var token_set_id = $(this).data("token-set-id");
-
         if (typeof token_set_id !== typeof undefined) {
           $(this).addClass("has-token-data");
-
-          $(this).find("textarea").click(function() {
+          var targetElement = $(this).find("textarea");
+          if (targetElement.length == 0) {
+            targetElement = $(this);
+          }
+          targetElement.filter("textarea").click(function() {
             Drupal.settings.token_set_last_selected_field = this;
             showTokens($(this));
+            $('.first-token-set').click();
+            $('.first-token-set').removeClass('first-token-set');
           });
         }
       });
@@ -24,59 +28,59 @@
 
         // Generate tokens markup.
         var last_token_type = '';
+        var headerHTML = '<div class="token-ui-header">';
         var html = '<fieldset id="token-set-tokens" class="form-wrapper">';
+        html += '<div class="token-set-tokens-spike">&nbsp;</div>';
+        html += '<div class="token-set-tokens-inner">';
         html += '<legend><span class="fieldset-legend">Tokens</span></legend>';
         html += '<div class="fieldset-wrapper token-set-wrapper">';
-        html += '<table>';
+        html += '<div class="token-list">';
+        var firstTokenSet = ' first-token-set';
         for (var i = 0; i < tokens.length; i++) {
           if (tokens[i].token_type !== last_token_type) {
-            html += '<tr><td colspan="3"><a class="token-set-expand" data-type="' + tokens[i].token_type + '" data-expanded="0" href="#">[+]</a>' + tokens[i].token_type + '</td></tr>';
+            headerHTML += '<a class="token-set-expand' + firstTokenSet + '" data-type="' + tokens[i].token_type.replace(' ', '-') + '" data-expanded="0" href="#">' + tokens[i].token_type + '</a>';
             last_token_type = tokens[i].token_type;
           }
-
+          firstTokenSet = '';
           html += renderToken(tokens[i]);
         }
-        html += '</div>';
-        html += '</table>';
+        headerHTML += '</div>';
+        html += '</div></div></div>';
         html += '</fieldset>';
-
         element.parent().append(html);
-
+        $('#token-set-tokens legend').after(headerHTML);
         $(".token-set-expand").each(function() {
           $(this).click(function(event) {
             event.preventDefault();
-
             var token_type = $(this).data("type");
-
-            if ($(this).hasClass("token-set-expanded")) {
-              $(".token-set-token-type-" + token_type).hide();
-              $(this).removeClass("token-set-expanded");
-              $(this).html("[+]");
-            } else {
-              $(".token-set-token-type-" + token_type).show();
-              $(this).addClass("token-set-expanded");
-              $(this).html("[-]");
-            }
+            $(".token-set-token-row").hide();
+            $(".token-set-token-type-" + token_type.replace(' ', '-')).show();
+            $(".token-set-expand").removeClass("token-set-expanded");
+            $(this).addClass("token-set-expanded");
           });
         });
 
-        $(".token-set-token-link").each(function() {
+        $('.token-set-token-row').each(function() {
           $(this).click(function(event) {
             event.preventDefault();
-
             var token = $(this).data("token");
-
             insertToken(Drupal.settings.token_set_last_selected_field, token);
           });
         });
       };
 
+      $('.token-set-token-row').click(function () {
+        event.preventDefault();
+        $(this).children('a.token-set-token-link').click();
+      });
+
+
       var renderToken = function (token) {
-        var html = '<tr class="token-set-token-row token-set-token-type-' + token.token_type + '">';
-        html += '<td>' + token.token_type + '</td>';
-        html += '<td><a class="token-set-token-link" data-token="' + token.token + '" href="#">' + token.token + '</a></td>';
-        html += '<td>' + token.token_description + '</td>';
-        html += '</tr>';
+        var html = '<div class="token-set-token-row token-set-token-type-' + token.token_type.replace(' ', '-') + '" data-token="' + token.token + '">';
+        html += '<div class="token-col token-machine">' + token.token + '</div>';
+        html += '<div class="token-col token-descr">' + token.token_description + '</div>';
+        html += '<div class="token-col token-use">use token</div>';
+        html += '</div>';
         return html;
       };
 
