534d2b00a00b5579761805136094fd23bdd2b208
klapalme <kl.jr@aol.com>
2016-05-23 09:24:57 -0400

    tweak to bundler sorting

--- fundraiser/modules/fundraiser_designations/js/bundler.js
+++ fundraiser/modules/fundraiser_designations/js/bundler.js
@@ -1,44 +1,50 @@
-(function($) {
-    Drupal.behaviors.WebformIPE = {
+(function ($) {
+    Drupal.behaviors.fdBundler = {
         attach: function (context, settings) {
-            $(window).ready(function() {
+            $(window).ready(function () {
                 window.App = {
-                    Handlers: {},
+                    Handlers: {}
                 };
+                App.Handlers.reorderItems = function () {
 
-                $('#fundraiser-designations-categories table').draggable({
-                    connectToSortable: "#fundraiser-designations-groups",
-                    helper: "clone",
-                    revert: false,
-                    placeholder: 'sortable-placeholder',
-                    cursorAt: { left: 5 }
-                });
+                    $('#fundraiser-designations-categories table').draggable({
+                        connectToSortable: "#fundraiser-designations-groups",
+                        helper: "clone",
+                        revert: false,
+                        placeholder: 'sortable-placeholder',
+                        cursorAt: {left: 5},
+                        stop: function (event, ui) {
+                            App.Handlers.reorderItems();
+                        }
+                    });
 
-                $( ".designations-category-table tr").draggable({
-                    connectToSortable: "#fundraiser-designations-groups table",
-                    helper: "clone",
-                    revert: false,
-                    cancel: 'th',
-                    placeholder: 'sortable-placeholder',
-                    cursorAt: { left: 5 }
-                });
+                    $(".designations-category-table tr").draggable({
+                        connectToSortable: "#fundraiser-designations-groups table",
+                        helper: "clone",
+                        revert: false,
+                        cancel: 'th',
+                        placeholder: 'sortable-placeholder',
+                        cursorAt: {left: 5},
+                        stop: function (event, ui) {
+                            App.Handlers.reorderItems();
+                        }
+                    });
 
-                $( "#fundraiser-designations-groups table tr").draggable({
-                    connectToSortable: "#fundraiser-designations-groups table",
-                    helper: "original",
-                    revert: false,
-                    cancel: 'th',
-                    placeholder: 'sortable-placeholder',
-                    cursorAt: { left: 5 }
-                });
+                    $("#fundraiser-designations-groups table tr").draggable({
+                        connectToSortable: "#fundraiser-designations-groups table",
+                        helper: "original",
+                        revert: false,
+                        cancel: 'th',
+                        placeholder: 'sortable-placeholder',
+                        cursorAt: {left: 5}
+                    });
 
-                App.Handlers.reorderItems = function() {
 
                     $('#fundraiser-designations-groups table').sortable({
                         cancel: 'th, thead',
                         placeholder: 'sortable-placeholder',
-                        items: 'tr',
-                        cursorAt: { left: 5 },
+                        items: 'tr:not("thead tr")',
+                        cursorAt: {left: 5},
                         start: function (event, ui) {
                         },
                         stop: function (event, ui) {
@@ -49,7 +55,7 @@
                     $('#fundraiser-designations-groups').sortable({
                         placeholder: 'sortable-placeholder',
                         items: 'table',
-                        cursorAt: { left: 5 },
+                        cursorAt: {left: 5},
                         start: function (event, ui) {
                         },
                         stop: function (event, ui) {
