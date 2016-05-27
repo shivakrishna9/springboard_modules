27c3809170ff85205e1b80b100fbaee23b8ba75b
Robert Romore <robert.romore@jacksonriver.com>
2016-05-27 15:15:16 -0500

    1034: Don’t store the expiration message in the schema, but instead store the message and format and process the text on node view; change theme name to reflect expiration instead of closed; add template for expiration message

---
+++ fundraiser/templates/fundraiser-expiration-message.tpl.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Template file to customize a fundraiser's expiration message.
+ *
+ * Available variables:
+ * - $message: The expiration message.
+ */
+?>
+
+<?php if ($message): ?>
+  <div class="fundraiser-expiration-message">
+    <?php print $message; ?>
+  </div>
+<?php endif; ?>
