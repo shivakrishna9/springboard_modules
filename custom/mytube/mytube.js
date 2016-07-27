bc63ff041cb86337a95075b6ec1400f69d82770c
Shaun Brown <shaun.brown@jacksonriver.com>
2016-07-27 12:48:06 -0700

    3: Apply mytube-fix-tabbing patch

--- custom/mytube/mytube.js
+++ custom/mytube/mytube.js
@@ -2,17 +2,25 @@
 
 Drupal.behaviors.mytube = {
   attach: function (context, settings) {
-    $('.mytubetrigger').once('mytubetrigger').click(function(){
+    $('.mytubetrigger', context).once('mytubetrigger').click(function(){
       $(this).hide();
       $(this).after(unescape($('.mytubeembedcode', this).html()));
       Drupal.attachBehaviors(this);
-
+      
       // If API usage is enabled, instantiate the API.
       if (Drupal.settings.mytube.enable_js_api === 1) {
         Drupal.behaviors.mytube.InitiateYouTubeAPI();
       }
 
     });
+    
+    // Start the video when pressing the Enter button
+    $('.mytubetrigger', context).keypress(function(e){
+      if(e.which == 13){ // Enter key pressed
+        $(this).click(); // Trigger search button click event
+      }
+    });
+    
   }
 };
 
