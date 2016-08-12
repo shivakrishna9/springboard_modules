f9e4ac4a3b2216bf63193b48543217fb882f6748
klapalme <kl.jr@aol.com>
2016-08-12 08:26:28 -0400

    update social share javascript

--- springboard_social/scripts/sb_social.addthis.js
+++ springboard_social/scripts/sb_social.addthis.js
@@ -1,18 +1,22 @@
-
 (function ($) {
 
+  // Makes ajax call to social share endpoint clicking share link
+  // Grabs social share referrer URL and appends it to link
+  // Opens popup social window.
+  // Prevents double share due to repreated link clcking or popup blockers.
   Drupal.behaviors.sb_social = {
     attach: function (context, settings) {
-
       $('.social-share-link').each(function () {
-          var link = window.location.href;
-          // Add a class to ensure the click event handler is only attached
-          // a single time regardless of how many times attach is triggered.
+        var link = window.location.href;
+        // Drupal once to limit clicks on ajax page reload.
+        $(this).once('popup', function() {
           $(this).click(function (e) {
+            // Setup some vars we use later.
             var $elem = $(this);
-            // account for subdirectory install
             var $url = Drupal.settings.basePath;
             var service;
+
+            // Build social share ajax URLS.
             if ($(this).hasClass('facebook')) {
               $url += 'sb_social/share_event/facebook/';
               service = 'facebook';
@@ -28,28 +32,38 @@
             $url = $url + Drupal.settings.sb_social.id + '/';
             $url = $url + Drupal.settings.sb_social.id_type + '/';
             $url = $url + Drupal.settings.sb_social.market_source + '/';
-
-            // conditionally add webform submission id if present
             if (typeof Drupal.settings.sb_social.submission_id !== 'undefined') {
               $url = $url + '?sid=' + Drupal.settings.sb_social.submission_id;
             }
 
-            if (!$(this).hasClass('social-processed')) {
-              // Register share event and provide share URL to AddThis.
-              jQuery.ajax({
-                url: $url,
-                success: function (response) {
-                  if (response.indexOf("http") === -1) {
-                    response = 'https://' + response;
-                  }
-                  link = encodeURIComponent(response);
-                },
-                async: false
-              });
-            }
+            // Make the ajax call to register the share with Drupal (and Salesforce).
+            jQuery.ajax({
+              beforeSend: function () {
+                // Don't do the ajax call if this link has already been clicked
+                // and the popup window has been blocked or is still open.
+                if ($elem.hasClass('social-processed')) {
+                  return false;
+                }
+              },
+              url: $url,
+              success: function (response) {
+                // The response is a link to the page, with a social
+                // share parameter appended.
+                // The shorten module has an option to 'www' instead of 'http'
+                // which breaks twitter integration. add http to links.
+                if (response.indexOf("http") === -1) {
+                  response = 'https://' + response;
+                }
+                link = encodeURIComponent(response);
+              },
+              // Don't use async because we have to wait for the updated link.
+              async: false
+            });
 
+            // Mark this link as clicked.
             $(this).addClass('social-processed');
 
+            //Open the popup or email app.
             var settings = Drupal.settings.sb_social;
             switch (service) {
               case 'twitter':
@@ -64,41 +78,60 @@
                 break;
               case 'email':
                 $elem.attr('href', ' mailto:?subject=' + settings.email_subject + '&body=' + settings.email_message + "\n\n" + link);
+                $(this).removeClass('social-processed');
                 break;
             }
-
           });
-
+        });
       });
     }
   };
 
-  var socialPopup = function(item, path) {
+  // Popup opener.
+  var socialPopup = function (item, path) {
 
     if (!path) {
       console.log("path must not be empty");
+      return false;
     }
-    var options = $.extend({
-      windowName: 'sbSocial',
-      windowOptions: 'width=550,height=420',
-      callback: function(item){
-        $(item).removeClass('social-processed');
-      }
-    }, options);
 
+    var options = {
+      windowName: 'sbSocialPopup',
+      windowOptions: 'width=550,height=420',
+      callback: function (item) {
+        // The callback function gets fired (by setInterval) when the window
+        // is closed, thereby re-enabling subsequent ajax calls
+        // to get new social share IDs.
+        $(item).removeClass('social-processed');
+        window.sbSocialPopup = null;
+      }
+    };
+
+    // If the window already exists, just update the location, otherwise
+    // open one.
     if (window.sbSocialPopup) {
-      window.sbSocialPopup.close();
+      // Firefox won't focus an already open window unless you close it first.
+      if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+        window.clearInterval(window.oauthInterval);
+        window.sbSocialPopup.close();
+        window.sbSocialPopup = window.open(path, options.windowName, options.windowOptions);
+      }
+      else {
+        window.sbSocialPopup.location.href = path;
+      }
     }
-    window.sbSocialPopup = window.open(path, options.windowName, options.windowOptions);
+    else {
+      window.sbSocialPopup = window.open(path, options.windowName, options.windowOptions);
+    }
     window.sbSocialPopup.focus();
 
-     //If the window is closed, re-enable the social share click event
-     //that was disabled previously to prevent double clicks.
+    //If the window is closed, re-enable the social share click event
+    //that was previously disabled to prevent double clicks.
     var x = 0;
-    var oauthInterval = window.setInterval(function(){
+    window.oauthInterval = window.setInterval(function () {
       x++;
-      if (window.sbSocialPopup.closed || x === 60) {
-        window.clearInterval(oauthInterval);
+      if ((window.sbSocialPopup && window.sbSocialPopup.closed) || x === 30) {
+        window.clearInterval(window.oauthInterval);
         options.callback(item);
       }
     }, 1000, x);
