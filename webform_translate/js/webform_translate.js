7b3067c7ca7e334ff9d18a453bd57216b30853ec
Josh Estep <josh.estep@jacksonriver.com>
2016-08-29 18:32:22 -0400

    Re-worked translation manager form to only allow one translation language column at a time, defaulting to the language of the current user; also added label rows above each new field; associated css adjustments

---
+++ webform_translate/js/webform_translate.js
@@ -0,0 +1,34 @@
+/**
+ * @file
+ * js for Webform Translate
+ */
+
+Drupal.behaviors.webformTranslateManagerBehavior = {
+  attach: function(context) { (function($) {
+    // Expand fields on focus:
+    $('#webform-translate-translation-manager-form .form-item.form-type-textfield input').each(function () {
+      $(this).focus(function () {
+        $(this).addClass('focus-active');
+      });
+      $(this).blur(function () {
+        $(this).removeClass('focus-active');
+      });
+    });
+
+    // Alter the language filter so the user's current langauge is selected and undesired options are removed:
+    var targetLanguage = $('.view-webform-translate-translation-manager .view-filters #edit-language').attr('default_lang_code');
+    $('.view-webform-translate-translation-manager #edit-language option').each(function () {
+      if ($(this).val() == 'All'
+        || $(this).val() == 'en'
+        || $(this).val() == '***CURRENT_LANGUAGE***'
+        || $(this).val() == '***DEFAULT_LANGUAGE***'
+        || $(this).val() == 'und') {
+        $(this).remove();
+      }
+      else if ($(this).val() == targetLanguage) {
+        $(this).attr('selected', 'selected');
+      }
+    });
+  })(jQuery); }
+  
+}
