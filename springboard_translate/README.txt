2f304adaec21d534e82d726023d4beb519f41f0f
Josh Estep <josh.estep@jacksonriver.com>
2016-08-31 13:50:10 -0400

    Renamed webform_translate to sb_translate as we are making translations node-specific to nodes that may not always be webforms.

---
+++ springboard_translate/README.txt
@@ -0,0 +1,7 @@
+The Springboard Translate module does the following:
+
+- Register each webform node's components and other webform-related output for i18n-based translation.
+
+- Provides a node-specific view on which all of a webform's translations can be managed.
+
+- Alters the output of webform components to display the appropriate language according to the Locale module.
---
+++ springboard_translate/js/sb_translate.js
@@ -0,0 +1,45 @@
+/**
+ * @file
+ * js for Webform Translate
+ */
+
+Drupal.behaviors.webformTranslateManagerBehavior = {
+  attach: function(context) { (function($) {
+    // Expand fields on focus:
+    $('#webform-translate-translation-manager-form .form-item.form-type-textfield input').each(function () {
+      $(this).keydown(function () {
+        if ($(this).val().length > 32) {
+          $(this).addClass('focus-active');
+        } else {
+          $(this).removeClass('focus-active');
+        }
+      });
+      $(this).focus(function () {
+        if ($(this).val().length > 32) {
+          $(this).addClass('focus-active');
+        }
+      });
+      $(this).blur(function () {
+        $(this).removeClass('focus-active');
+      });
+    });
+    // Show or hide state options based on currently selected country:
+    $('.view-webform-translate-translation-manager .trans-manager-state-selector').change(function () {
+      _transManagerUpdateStateOptions($(this).val());
+    });
+    _transManagerUpdateStateOptions($('.view-webform-translate-translation-manager .trans-manager-state-selector').val());
+    function _transManagerUpdateStateOptions(newCountry) {
+      $('#webform-translation-manager .form-type-textfield input').each(function () {
+        if ($(this).attr('country_code') != undefined) {
+          if (newCountry != 'all-countries' && $(this).attr('country_code') != newCountry) {
+            $(this).parent().parent().parent().hide();
+          }
+          else {
+            $(this).parent().parent().parent().show();
+          }
+        }
+      });
+    }
+  })(jQuery); }
+  
+}
