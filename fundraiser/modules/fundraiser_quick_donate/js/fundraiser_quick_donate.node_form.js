08b9af39348bd2ca3fed31e99175ad71b7b871e8
Robert Romore <robert.romore@jacksonriver.com>
2016-09-23 13:19:48 -0500

    936/1368: Updating payment method enabling warning note when payment method not supported

--- fundraiser/modules/fundraiser_quick_donate/js/fundraiser_quick_donate.node_form.js
+++ fundraiser/modules/fundraiser_quick_donate/js/fundraiser_quick_donate.node_form.js
@@ -39,7 +39,7 @@
           $quickdonate.find('input[type=checkbox]').attr('disabled', 'disabled').attr('checked', false);
           $quickdonate.nextAll('.form-item, .form-wrapper').hide();
           if ($quickdonate.next('.note').length == 0) {
-            $quickdonate.after('<div class="note"><strong>Note:</strong> One or more of the payment gateways you have selected are not compatible with or configured for Quick Donate. Please finish configuring or remove the gateway(s) below in order to enable Quick Donate on this form: <ul><li>' + $option_name + '</li></ul><br/><br/></div>');
+            $quickdonate.after('<div class="note"><strong>Note:</strong> One or more of the payment gateways you have selected are not compatible with or configured for Quick Donate. Donors will not be able to opt-in to Quick Donate when using this payment method: <ul><li>' + $option_name + '</li></ul><br/><br/></div>');
           }
           else {
             $quickdonate.next('.note').find('ul').append('<li>$option_name</li>');
