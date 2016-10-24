51421022ea0696b9993b2088e67e60c85ab5e7c4
Scott Phillips <scott.phillips@jacksonriver.com>
2016-10-24 15:31:32 -0500

    Adding stubbed salesforce_genmap.api.php

---
+++ salesforce/salesforce_genmap/salesforce_genmap.api.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @file
+ * These are the hooks that are invoked by salesforce_genmap.
+ */
+
+/**
+ * @defgroup salesforce_genmap_hooks Salesforce Generic Hooks
+ * @{
+ * Salesforce Generic's hooks
+ */
+
+function hook_salesforce_genmap_delete_map($nid, $module) {
+
+}
+
+function hook_salesforce_genmap_info() {
+
+}
+
+function hook_salesforce_genmap_info_alter($modules) {
+
+}
+
+function hook_salesforce_genmap_map_field_info($node) {
+
+}
+
+function hook_salesforce_genmap_map_field_info_alter($fields, $node, $module) {
+
+}
+
+
+function hook_salesforce_genmap_map_fields($object, $map, $node, $op) {
+
+}
+
+function hook_salesforce_genmap_map_fields_alter($fields, $context) {
+
+}
+
+function hook_salesforce_genmap_map_item_alter($item, $drupal_object) {
+
+}
+
+function hook_salesforce_genmap_map_object_alter($sobject) {
+
+}
+
+function hook_salesforce_genmap_permission() {
+
+}
+
+function hook_salesforce_mapping_map_fields_alter($fields, $context){
+
+}
+
+function hook_salesforce_mapping_entity_fields_alter($entity_fields, $entity_type, $entity_bundle_type) {
+
+}
