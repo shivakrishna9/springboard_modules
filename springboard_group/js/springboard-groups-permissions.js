9f4e68f58171b3384424da6b64e7884e4235abfc
klapalme <kl.jr@aol.com>
2016-11-03 14:21:37 -0400

    add content types to custom permisisons screen

--- springboard_group/js/springboard-groups-permissions.js
+++ springboard_group/js/springboard-groups-permissions.js
@@ -1,32 +1,87 @@
-
 (function ($) {
   Drupal.behaviors.SpringboardGroupPermissionsForm = {
-    attach: function(context, settings) {
+    attach: function (context, settings) {
+      if ($.cookie('Drupal.groupPermissions.showHidden') == 0 || $.cookie('Drupal.groupPermissions.showHidden') === null) {
+        $('table#permissions').before('<a href="#" class="show-content-type show-hidden-fields-off">Show Content type permissions</a>');
+      }
+      else {
+        $('table#permissions').before('<a href="#" class="show-content-type show-hidden-fields-on">Hide Content type permissions</a>');
+      }
+      $('.show-content-type').css('float','right');
+      $('.show-content-type').click(function() {
+        if ($(this).hasClass('show-hidden-fields-on')) {
+          $(this).removeClass('show-hidden-fields-on').addClass('show-hidden-fields-off').text('Show content type permssions');
+          $.cookie('Drupal.groupPermissions.showHidden', 0, {
+            path: Drupal.settings.basePath,
+            // The cookie expires in one year.
+            expires: 365
+          });
+        }
+        else {
+          $(this).removeClass('show-hidden-fields-off').addClass('show-hidden-fields-on').text('Hide Content type permissions');
+          $.cookie('Drupal.groupPermissions.showHidden', 1, {
+            path: Drupal.settings.basePath,
+            // The cookie expires in one year.
+            expires: 365
+          });
+          console.log($.cookie('Drupal.groupPermissions.showHidden'));
+
+        }
+
+        $('table#permissions tr').each(function(){
+          if ($(this).hasClass('hidden-component-off')) {
+            $(this).removeClass('hidden-component-off').addClass('hidden-component-on');
+            $(this).children('td:first:contains("new content")').parents('tr').show();
+            $(this).children('td:first:contains("own content")').parents('tr').show();
+            $(this).children('td:first:contains("any content")').parents('tr').show();
+          }
+          else {
+            $(this).removeClass('hidden-component-on').addClass('hidden-component-off');
+            $(this).children('td:first:contains("new content")').parents('tr').hide();
+            $(this).children('td:first:contains("own content")').parents('tr').hide();
+            $(this).children('td:first:contains("any content")').parents('tr').hide();
+          }
+        });
+        return false;
+      });
       var rows = $('table#permissions tr');
       var hidem = false;
       // Hide everything but the rows necessary to administer organization issues.
-      rows.each(function(){
+      rows.each(function () {
         var id = $(this).children('td:first').attr('id');
-        if ($(this).children('td:first').hasClass('module') && (id !== 'springboard-group' && id !== 'module-springboard_group' && id !== 'module-og' && id !== 'module-node')) {
+        var allow = [
+          'springboard-group',
+          'module-springboard_group',
+          'module-og',
+          'module-node',
+          'module-publishcontent',
+          'module-rules'
+        ];
+        if ($(this).children('td:first').hasClass('module') && $.inArray(id, allow) === -1) {
           hidem = true;
         }
         else if ($(this).children('td:first').hasClass('module')) {
           hidem = false;
         }
-        if(hidem === true) {
+        if (hidem === true) {
           $(this).hide();
         }
         else {
-            $(this).children('td:first:contains("new content")').parents('tr').hide();
-            $(this).children('td:first:contains("own content")').parents('tr').hide();
-            $(this).children('td:first:contains("any content")').parents('tr').hide();
-            $(this).children('td:first:contains("Administer content")').parents('tr').hide();
-            $(this).children('td:first:contains("revisions")').parents('tr').hide();
-            $(this).children('td:first:contains("content overview page")').parents('tr').hide();
-            $(this).children('td:first:contains("View published content")').parents('tr').hide();
-            $(this).children('td:first:contains("View own unpublished")').parents('tr').hide();
-            $(this).children('td:first:contains("Administer content types")').parents('tr').hide();
-            $(this).children('td:first:contains("Springboard Group")').parents('tr').show();
+          console.log($.cookie('Drupal.groupPermissions.showHidden'));
+          if ($.cookie('Drupal.groupPermissions.showHidden') == 0 || $.cookie('Drupal.groupPermissions.showHidden') === null) {
+            $(this).children('td:first:contains("new content")').parents('tr').hide().addClass('hidden-component-off');
+            $(this).children('td:first:contains("own content")').parents('tr').hide().addClass('hidden-component-off');
+            $(this).children('td:first:contains("any content")').parents('tr').hide().addClass('hidden-component-off');
+          }
+          $(this).children('td:first:contains("Administer content")').parents('tr').hide();
+          $(this).children('td:first:contains("revisions")').parents('tr').hide();
+          $(this).children('td:first:contains("content overview page")').parents('tr').hide();
+          $(this).children('td:first:contains("View published content")').parents('tr').hide();
+          $(this).children('td:first:contains("Access the Rules debug log")').parents('tr').hide();
+          $(this).children('td:first:contains("Use Rules component add_og_membership")').parents('tr').hide();
+          $(this).children('td:first:contains("View own unpublished")').parents('tr').hide();
+          $(this).children('td:first:contains("Administer content types")').parents('tr').hide();
+          $(this).children('td:first:contains("Springboard Group")').parents('tr').show();
         }
       });
     }
