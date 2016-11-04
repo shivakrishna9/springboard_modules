8680cb1897163cb73402aed2629a51f1ba18c8ef
Josh Estep <josh.estep@jacksonriver.com>
2016-11-04 13:23:21 -0400

    Added help popup message; added font awesome-based icons; made calendar icons display or hide the calendar on click

--- springboard_dw/js/springboard_dw.js
+++ springboard_dw/js/springboard_dw.js
@@ -8,16 +8,41 @@ Drupal.behaviors.springboardDataWarehouseViews = {
     // Attach help popup to the right of the submit button:
     var submitButton = $('#views-exposed-form-springboard-dw-donations-report-page .views-submit-button input');
     var donationsReportTable = $('.view-springboard-dw-donations-report table.views-table');
-    submitButton.after('<span id="sb-dw-donations-report-help"> ? </span>');
+    submitButton.after('<span id="sb-dw-donations-report-help"> <i class="fa fa-question-circle-o fa-lg"></i> </span>');
     $('#sb-dw-donations-report-help').click(function (e) {
       e.preventDefault();
-      donationsReportTable.before('<div id="sb-dw-donations-report-help-text">' +
-        'Use this form to search archived Donation data.  Please note that it take up to 15 minutes for website donation data to appear in this archive.</div>');
-      $(this).hide();
+      if ($('#sb-dw-donations-report-help-popup').length) {
+        $('#sb-dw-donations-report-help-popup').fadeOut('slow', function () {
+          $('#sb-dw-donations-report-help-popup').remove();
+        });
+      }
+      else {
+        $(this).after('<div id="sb-dw-donations-report-help-popup" style="display:none;">' +
+          'Use this form to view all donation data in the warehouse.  ' +
+          'Click on the columns to sort results or apply filters to the search and hit Go.  ' +
+          'Please note that it take up to 15 minutes for website donation data to appear in the data warehouse.</div>');
+        $('#sb-dw-donations-report-help-popup').fadeIn('slow');
+        $('#sb-dw-donations-report-help-popup').click(function () {
+          $('#sb-dw-donations-report-help-popup').fadeOut('slow', function () {
+            $('#sb-dw-donations-report-help-popup').remove();
+          });
+        });
+      }
     });
 
     // Alter date range filter markup:
-    $('#views-exposed-form-springboard-dw-donations-report-page #edit-transaction-date-min input').after('<span id="dw-date-range-separator">and</span>');
+    $('#views-exposed-form-springboard-dw-donations-report-page #edit-transaction-date-min input').after('<i class="fa fa-calendar dw-calendar-icon"></i> ' +
+      '<span id="dw-date-range-separator">and:</span>');
+    $('#views-exposed-form-springboard-dw-donations-report-page #edit-transaction-date-max input').after('<i class="fa fa-calendar dw-calendar-icon"></i>');
+    $('#views-exposed-form-springboard-dw-donations-report-page i.dw-calendar-icon').click(function () {
+      if ($('#ui-datepicker-div').length && !$('#ui-datepicker-div').is(':hidden')) {
+        $(this).parent().children('input').blur();
+      }
+      else {
+        $(this).parent().children('input').focus();
+        $(this).parent().children('input').click();
+      }
+    });
 
     // Rework presentation of the items per page element:
     var itemsPerPage = $('#views-exposed-form-springboard-dw-donations-report-page #edit-items-per-page');
