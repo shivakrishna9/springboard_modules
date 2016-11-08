251a5659ba332f7ef1eab6261453913cb9eeb39a
Scott Phillips <scott.phillips@jacksonriver.com>
2016-11-08 12:53:28 -0600

    Creates hook_salesforce_sync_save_map_alter() and prevents batch_upload objects from being saved to salesforce_sync_map.

--- salesforce/salesforce_sync/salesforce_sync.api.php
+++ salesforce/salesforce_sync/salesforce_sync.api.php
@@ -51,5 +51,15 @@ function hook_salesforce_sync_fail_batch($batch, $message, $result) {
 }
 
 /**
+ * Fire off behavior before salesforce_sync_map entity is saved.
+ *
+ * @param mixed $record
+ *   Can be an array of values or a salesforce_sync_map entity object.
+ */
+function hook_salesforce_sync_save_map_alter($record) {
+
+}
+
+/**
  * @}
  */
