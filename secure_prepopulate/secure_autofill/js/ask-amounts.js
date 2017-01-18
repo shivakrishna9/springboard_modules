56d40e50173ca88acc567e29e619180e0600d7ab
Matthew Thomas <matt.thomas@jacksonriver.com>
2017-01-18 18:08:06 -0500

    altering to work with and without other amounts

--- secure_prepopulate/secure_autofill/js/ask-amounts.js
+++ secure_prepopulate/secure_autofill/js/ask-amounts.js
@@ -30,32 +30,38 @@
             // recurring_default: "17"
             if (typeof(data.content.amounts) !== 'undefined') {
               // Find and replace amount.
-              // First, select and remove all options that aren't 'other'
-              $('#edit-submitted-donation-amount input[value!=other]').parent('.form-item').remove();
+              // First, hide all regular options from the form.
+              $('#edit-submitted-donation-amount .form-item').hide();
+              // If the last amount's value is 'other', show it.
+              if ($('#edit-submitted-donation-amount .form-item').last().children('input').val() == 'other') {
+                $('#edit-submitted-donation-amount .form-item').last().show();
+              }
               // Next, iterate and process amounts.
               var amounts = data.content.amounts.split("|");
               for (var i = 0, len = amounts.length; i < len; i++) {
                 // Take the 'other' form item, alter its label and value, and insert before the 'other' form item.
-                var otherAmount = $('#edit-submitted-donation-amount input[value=other]').parent('.form-item').clone(true);
+                var lastAmount = $('#edit-submitted-donation-amount .form-item').last().clone(true);
                 // Set amount value.
-                $(otherAmount).children('input').val(amounts[i]);
+                $(lastAmount).children('input').val(amounts[i]);
                 // Set amount label.
                 // TODO use appropriate currency.
-                $(otherAmount).children('label').text('$' + amounts[i]);
+                $(lastAmount).children('label').text('$' + amounts[i]);
                 // Set element ID.
-                otherAmount.attr('id', "edit-submitted-donation-amount-" + i);
-                // Insert before 'other' amount.
-                $('#edit-submitted-donation-amount input[value=other]').parent('.form-item').before(otherAmount);
+                lastAmount.attr('id', "edit-submitted-donation-amount-" + i);
+                // Insert before last amount.
+                console.log(lastAmount);
+                $('#edit-submitted-donation-amount .form-item').last().before(lastAmount);
+                // Show the new amount.
+                $(lastAmount).show();
               }
             }
             // Set 'default' amount.
             if (typeof(data.content.default) !== 'undefined') {
               // Get the default amount as returned and attempt to set it.
-              var amounts = $('#edit-submitted-donation-amount .form-item input');
+              var amounts = $('#edit-submitted-donation-amount .form-item input:visible');
               // Iterate through amounts, set value if possible.
               for (var i = 0, len = amounts.length; i < len; i++) {
                 if (amounts[i].value == data.content.default) {
-                  console.log("I FOUND THE DEFAULT");
                   // Uncheck all other values first.
                   $('#edit-submitted-donation-amount .form-item input').attr('checked', false);
                   // Now set ours.
@@ -65,8 +71,51 @@
                 }
               }
             }
-            // TODO set other amounts
-            // TODO set other default
+            // Set recurring ask amounts.
+            if (typeof(data.content.recurring_amounts) !== 'undefined') {
+              // Find and replace amount.
+              // First, hide all regular options from the form.
+              $('#edit-submitted-donation-recurring-amount .form-item').hide();
+              // If the last amount's value is 'other', show it.
+              if ($('#edit-submitted-donation-recurring-amount .form-item').last().children('input').val() == 'other') {
+                $('#edit-submitted-donation-recurring-amount .form-item').last().show();
+              }
+              // Next, iterate and process recurring_amounts.
+              var recurring_amounts = data.content.recurring_amounts.split("|");
+              for (var i = 0, len = recurring_amounts.length; i < len; i++) {
+                // Take the 'other' form item, alter its label and value, and insert before the 'other' form item.
+                var lastAmount = $('#edit-submitted-donation-recurring-amount .form-item').last().clone(true);
+                // Set amount value.
+                $(lastAmount).children('input').val(recurring_amounts[i]);
+                // Set amount label.
+                // TODO use appropriate currency.
+                $(lastAmount).children('label').text('$' + recurring_amounts[i]);
+                // Set element ID.
+                lastAmount.attr('id', "edit-submitted-donation-recurring-amount-" + i);
+                // Insert before last amount.
+                console.log(lastAmount);
+                $('#edit-submitted-donation-recurring-amount .form-item').last().before(lastAmount);
+                // Show the new amount.
+                $(lastAmount).show();
+              }
+            }
+            // Set recurring default.
+            // TODO this needs to work on change to recurring.
+            if (typeof(data.content.recurring_default) !== 'undefined') {
+              // Get the recurring_default amount as returned and attempt to set it.
+              var amounts = $('#edit-submitted-donation-recurring-amount .form-item input');
+              // Iterate through amounts, set value if possible.
+              for (var i = 0, len = amounts.length; i < len; i++) {
+                if (amounts[i].value == data.content.recurring_default) {
+                  // Uncheck all other values first.
+                  $('#edit-submitted-donation-recurring-amount .form-item input').attr('checked', false);
+                  // Now set ours.
+                  $(amounts[i]).attr('checked', 'checked');
+                  // No need to continue.
+                  break;
+                }
+              }
+            }
           }
         });
       }
